3333c06f83e3dce1709221b6d28e55ab9326a768
Touch13 <ccr@adiczion.com>
2013-03-16 13:38:07 +0100

    [ADD] French translation of some files.

---
+++ core/components/minishop2/lexicon/fr/manager.inc.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Manager French Lexicon Entries for miniShop2
+ *
+ * @package minishop2
+ * @subpackage lexicon
+ */
+$_lang['ms2_menu_create'] = 'Créer';
+$_lang['ms2_menu_update'] = 'Mettre à jour';
+$_lang['ms2_menu_remove'] = 'Supprimer';
+$_lang['ms2_menu_remove_multiple'] = 'Supprimer la sélection';
+$_lang['ms2_menu_remove_confirm'] = 'Êtes vous sûr de vouloir supprimer la sélection ?';
+$_lang['ms2_menu_remove_multiple_confirm'] = 'Êtes vous sûr de vouloir supprimer toutes les entrées sélectionnées ?';
+
+$_lang['ms2_combo_select'] = 'Cliquer pour sélectionner';
+$_lang['ms2_combo_select_status'] = 'Filtrer par l\'état';
+
+$_lang['ms2_id'] = 'Id';
+$_lang['ms2_name'] = 'Nom';
+$_lang['ms2_color'] = 'Couleur';
+$_lang['ms2_country'] = 'Pays';
+$_lang['ms2_logo'] = 'Logo';
+$_lang['ms2_address'] = 'Adresse';
+$_lang['ms2_phone'] = 'Téléphone';
+$_lang['ms2_fax'] = 'Fax';
+$_lang['ms2_email'] = 'Courriel';
+$_lang['ms2_active'] = 'Actif';
+$_lang['ms2_class'] = 'Gestionnaire de classe';
+$_lang['ms2_description'] = 'Description';
+$_lang['ms2_num'] = 'Nombre';
+$_lang['ms2_status'] = 'États';
+$_lang['ms2_count'] = 'Compter';
+$_lang['ms2_cost'] = 'Coût';
+$_lang['ms2_order_cost'] = 'Montant de la commande';
+$_lang['ms2_cart_cost'] = 'Coût des produits';
+$_lang['ms2_delivery_cost'] = 'Coût de la livraison';
+$_lang['ms2_weight'] = 'Poids';
+$_lang['ms2_createdon'] = 'Créé le';
+$_lang['ms2_updatedon'] = 'Mis a jour le';
+$_lang['ms2_user'] = 'Utilisateur';
+$_lang['ms2_timestamp'] = 'Horodatage';
+$_lang['ms2_order_log'] = 'Journal de commande';
+$_lang['ms2_order_products'] = 'Articles';
+$_lang['ms2_action'] = 'Action';
+$_lang['ms2_entry'] = 'Écriture';
+$_lang['ms2_username'] = 'Nom de l\'utilisateur';
+$_lang['ms2_fullname'] = 'Nom complet';
+
+$_lang['ms2_receiver'] = 'Destinnataire';
+$_lang['ms2_index'] = 'Code postal';
+$_lang['ms2_region'] = 'Région';
+$_lang['ms2_city'] = 'Ville';
+$_lang['ms2_metro'] = 'Métro';
+$_lang['ms2_street'] = 'Rue';
+$_lang['ms2_building'] = 'Immeuble';
+$_lang['ms2_room'] = 'Appartement';
+$_lang['ms2_comment'] = 'Commentaire';
+
+$_lang['ms2_email_user'] = 'Message à l\'utilisateur';
+$_lang['ms2_email_manager'] = 'Message au responsable';
+$_lang['ms2_subject_user'] = 'Sujet du message pour l\'utilisateur';
+$_lang['ms2_subject_manager'] = 'Sujet du message pour le responsable';
+$_lang['ms2_body_user'] = 'Partie du message pour l\'utilisateur';
+$_lang['ms2_body_manager'] = 'Partie du message au responsable';
+$_lang['ms2_status_final'] = 'Finale';
+$_lang['ms2_status_final_help'] = '';
+$_lang['ms2_status_fixed'] = 'Correction d\'';
+$_lang['ms2_status_fixed_help'] = '';
+$_lang['ms2_options'] = 'Options';
+$_lang['ms2_price'] = 'Prix';
+$_lang['ms2_price_help'] = 'Coût minimal de la livraison';
+$_lang['ms2_weight_price'] = 'Prix par unité de poids';
+$_lang['ms2_weight_price_help'] = 'Coût supplémentaire par unité de poids.<br/>Peut être utilisé dans des classes personnalisées.';
+$_lang['ms2_distance_price'] = 'Prix par unité de distance';
+$_lang['ms2_distance_price_help'] = 'Coût supplémentaire par unité de distance.<br/>Peut être utilisé dans des classes personnalisées.';
+$_lang['ms2_order_requires'] = 'Champs requis';
+$_lang['ms2_order_requires_help'] = 'Lors de la commande, une classe personnalisée peut exiger le remplissage d\'un de ses champs';
+
+$_lang['ms2_orders_selected_status'] = 'Changer l\'état de la commande sélectionnée';
