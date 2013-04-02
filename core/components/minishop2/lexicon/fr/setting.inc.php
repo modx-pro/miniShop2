4ebefab220b83645c0a56694af4299e4e5bd2045
Touch13 <ccr@adiczion.com>
2013-04-02 23:49:16 +0200

    Small fix of the French translation.

---
+++ core/components/minishop2/lexicon/fr/setting.inc.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Settings English Lexicon Entries for miniShop2
+ *
+ * @package minishop2
+ * @subpackage lexicon
+ */
+$_lang['area_ms2_category'] = 'Catégorie d\'article';
+$_lang['area_ms2_product'] = 'Article';
+$_lang['area_ms2_gallery'] = 'Gallerie';
+$_lang['area_ms2_cart'] = 'Panier';
+$_lang['area_ms2_order'] = 'Commande';
+$_lang['area_ms2_frontend'] = 'Frontal';
+
+$_lang['setting_ms2_category_grid_fields'] = 'Champs de la tableau d\'articles';
+$_lang['setting_ms2_category_grid_fields_desc'] = 'Liste, séparée par des virgules, des champs visibles du tableau des articles dans la catégorie.';
+$_lang['setting_ms2_product_main_fields'] = 'Champs principaux du panneau de l\'article';
+$_lang['setting_ms2_product_main_fields_desc'] = 'Comma separated list of fields in the panel of the product. For example: "pagetitle,longtitle,content".';
+$_lang['setting_ms2_product_extra_fields'] = 'Extra fields of the panel of the product';
+$_lang['setting_ms2_product_extra_fields_desc'] = 'Comma separated list of fields in the panel of the product, that needed in your shop. For example: "price,old_price,weight".';
+
+$_lang['setting_ms2_category_show_comments'] = 'Display comments of the category';
+$_lang['setting_ms2_category_show_comments_desc'] = 'Display comments of all goods from category if component "Tickets" is installed.';
+$_lang['setting_ms2_product_show_comments'] = 'Display comments of the product';
+$_lang['setting_ms2_product_show_comments_desc'] = 'Display comments of the product if component "Tickets" is installed.';
+$_lang['setting_ms2_template_product_default'] = 'Default template for new product';
+$_lang['setting_ms2_template_product_default_desc'] = 'Select template which will be set by default when you creating new product.';
+$_lang['setting_ms2_product_show_in_tree_default'] = 'Show in tree by default';
+$_lang['setting_ms2_product_show_in_tree_default_desc'] = 'If you activate this option, all new goods will be shown in resource tree.';
+$_lang['setting_ms2_product_source_default'] = 'Default media source';
+$_lang['setting_ms2_product_source_default_desc'] = 'Default media source for the product gallery.';
+$_lang['setting_ms2_product_vertical_tabs'] = 'Vertical tabs at product page';
+$_lang['setting_ms2_product_vertical_tabs_desc'] = 'How to display product page in manager? Disabling this option allows you to fit the product page on the screen with a small horizontal size. Not recommended.';
+$_lang['ms2_source_thumbnails_desc'] = 'JSON encoded array of options for generating thumbnails.';
+
+$_lang['setting_ms2_cart_handler_class'] = 'Cart handler class';
+$_lang['setting_ms2_cart_handler_class_desc'] = 'The name of the class that implements the logic of a cart.';
+$_lang['setting_ms2_order_handler_class'] = 'Order handler class';
+$_lang['setting_ms2_order_handler_class_desc'] = 'The name of the class that implements the logic of an ordering.';
+$_lang['setting_ms2_order_user_groups'] = 'Groups for registering customers';
+$_lang['setting_ms2_order_user_groups_desc'] = 'Comma-separated list of user groups for adding new users when they orders.';
+$_lang['setting_ms2_email_manager'] = 'Managers mailboxes';
+$_lang['setting_ms2_email_manager_desc'] = 'Comma-separated list of mailboxes of managers, for sending notifications about changes of the status of the order';
+$_lang['setting_ms2_date_format'] = 'Format of dates';
+$_lang['setting_ms2_date_format_desc'] = 'You can specify how to format miniShop2 dates using php strftime() syntax.';
+
+$_lang['setting_ms2_frontend_css'] = 'Frontend styles';
+$_lang['setting_ms2_frontend_css_desc'] = 'Path to file with styles of the shop. If you want to use your own styles - specify them here, or clean this parameter and load them in site template.';
+$_lang['setting_ms2_frontend_js'] = 'Frontend scripts';
+$_lang['setting_ms2_frontend_js_desc'] = 'Path to file with scripts of the shop. If you want to use your own sscripts - specify them here, or clean this parameter and load them in site template.';
