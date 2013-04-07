b5e745ba3720e9160002e0cca364d02a8903a683
thedarki <ua13dark@gmail.com>
2013-04-07 22:31:33 +0300

    Update transport.policies.php

--- _build/data/transport.policies.php
+++ _build/data/transport.policies.php
@@ -14,7 +14,7 @@ $policies[0]->fromArray(array (
 	'description' => 'A policy for create and update miniShop categories and products.',
 	'parent' => 0,
 	'class' => '',
-	'lexicon' => 'tickets:permissions',
+	'lexicon' => 'minishop:permissions',
 	'data' => '{"mscategory_save":true,"msproduct_save":true,"load":true,"list":true,"view":true}',
 ), '', true, true);
 
