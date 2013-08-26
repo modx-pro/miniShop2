1f399137079e8f7299255d4f8bdb8b39eb4bafb9
bezumkin <bezumkin@yandex.ru>
2013-08-26 13:16:05 +0700

    Vesrion 2.1.0 beta2

--- _build/build.config.php
+++ _build/build.config.php
@@ -5,7 +5,7 @@ define('PKG_NAME', 'miniShop2');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 
 define('PKG_VERSION', '2.1.0');
-define('PKG_RELEASE', 'beta1');
+define('PKG_RELEASE', 'beta2');
 define('PKG_AUTO_INSTALL', true);
 
 /* define paths */
