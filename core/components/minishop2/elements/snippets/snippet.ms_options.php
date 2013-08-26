2cd16805190a366c02aa237b5031217d550cee84
bezumkin <bezumkin@yandex.ru>
2013-08-26 18:40:04 +0700

    Fixed snippet "msOptions"

--- core/components/minishop2/elements/snippets/snippet.ms_options.php
+++ core/components/minishop2/elements/snippets/snippet.ms_options.php
@@ -7,14 +7,14 @@ $pdoFetch = new pdoFetch($modx, $scriptProperties);
 if (empty($product) && !empty($input)) {$product = $input;}
 if (empty($selected)) {$selected = '';}
 if (empty($outputSeparator)) {$outputSeparator = "\n";}
-$name = (empty($name) || $name == 'id') && !empty($options) ? $options : '';
+if ((empty($name) || $name == 'id') && !empty($options)) {$name = $options;}
 
 $output = '';
 $product = !empty($product) ? $modx->getObject('msProduct', $product) : $product = $modx->resource;
 if (!($product instanceof msProduct)) {
 	$output = 'This resource is not instance of msProduct class.';
 }
-else if ($options = $product->get($name)) {
+elseif (!empty($name) && $options = $product->get($name)) {
 	if (!is_array($options) || $options[0] == '') {
 		$output = !empty($tplEmpty) ? $pdoFetch->getChunk($tplEmpty, $scriptProperties) : '';
 	}
@@ -31,7 +31,7 @@ else if ($options = $product->get($name)) {
 		$rows = implode($outputSeparator, $rows);
 		$output = empty($tplOuter)
 			? $pdoFetch->getChunk('', $rows)
-			: $pdoFetch->getChunk($tplOuter, $rows, array_merge($scriptProperties, array('name' => $name, 'rows' => $rows)));
+			: $pdoFetch->getChunk($tplOuter, array_merge($scriptProperties, array('name' => $name, 'rows' => $rows)));
 	}
 }
 
