7ad6465cba14f7a55dde08db2f75eedd93eb91b2
Bezumkin <bezumkin@yandex.ru>
2013-11-22 17:44:37 +0700

    Version 2.1.3-pl

--- _build/build.config.php
+++ _build/build.config.php
@@ -4,8 +4,8 @@
 define('PKG_NAME', 'miniShop2');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 
-define('PKG_VERSION', '2.1.2');
-define('PKG_RELEASE', 'pl2');
+define('PKG_VERSION', '2.1.3');
+define('PKG_RELEASE', 'pl');
 define('PKG_AUTO_INSTALL', true);
 
 /* define paths */
