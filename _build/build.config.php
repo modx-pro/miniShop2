f5e617d64248650b1816195bd4288afe9b841927
bezumkin <bezumkin@yandex.ru>
2014-12-27 14:33:00 +0700

    Improved chunk tpl.msOrder.payment Version 2.1.9-beta

--- _build/build.config.php
+++ _build/build.config.php
@@ -4,8 +4,8 @@
 define('PKG_NAME', 'miniShop2');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 
-define('PKG_VERSION', '2.1.8');
-define('PKG_RELEASE', 'pl3');
+define('PKG_VERSION', '2.1.9');
+define('PKG_RELEASE', 'beta');
 define('PKG_AUTO_INSTALL', true);
 
 /* define paths */
