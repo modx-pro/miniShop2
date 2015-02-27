03bfbadf2bf7278e2c0a557d9b9bbab4bf3448aa
soulcreate <faitnomor@gmail.com>
2015-02-27 16:18:32 +0600

    Add @INLINE support fot "tpl" mini cart

--- core/components/minishop2/elements/snippets/snippet.ms_minicart.php
+++ core/components/minishop2/elements/snippets/snippet.ms_minicart.php
@@ -7,4 +7,5 @@ $cart = $miniShop2->cart->status();
 $cart['total_cost'] = $miniShop2->formatPrice($cart['total_cost']);
 $cart['total_weight'] = $miniShop2->formatWeight($cart['total_weight']);
 
-return !empty($tpl) ? $modx->getChunk($tpl, $cart) : print_r($cart,1);
+if(!isset($pdo)) $pdo = $modx->getService('pdoTools');
+return !empty($tpl) ? $pdo->getChunk($tpl, $cart) : print_r($cart,1);
