b5ef7b15e1f57a4a749bcb7e6704c1dd3299cd24
Mark Willis <mark.willis@adi.do>
2015-12-08 13:05:31 +0000

    Updated member ranking on saves

--- core/components/minishop2/model/minishop2/mscategorymember.class.php
+++ core/components/minishop2/model/minishop2/mscategorymember.class.php
@@ -1,2 +1,25 @@
 <?php
-class msCategoryMember extends xPDOObject {}
+class msCategoryMember extends xPDOObject {
+
+	public function save($cacheFlag) {
+		$res = parent::save($cacheFlag);
+		if($this->get('rank') == 0) {
+			$max = 0;
+			$q = $this->xpdo->newQuery('msCategoryMember');
+			$q->where(array('category_id' => $this->get('category_id')));
+			$q->sortby('rank', 'DESC');
+			$q->limit(1);
+			$maxObj = $this->xpdo->getObject('msCategoryMember', $q);
+			if($maxObj) {
+				$max = $maxObj->get('rank') + 1;
+			}
+
+			$q = "UPDATE {$this->xpdo->getTableName('msCategoryMember')}
+				SET rank = {$max} WHERE
+					product_id = {$this->get('product_id')}
+					AND category_id = {$this->get('category_id')}";
+			$this->xpdo->exec($q);
+		}
+		return $res;
+	}
+}
