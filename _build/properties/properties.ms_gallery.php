36d217500c3043ac8fd26bb3717b933b8934b546
Vasily Naumkin <bezumkin@yandex.ru>
2015-12-14 14:44:43 +0700

    Updated builder

--- _build/properties/properties.ms_gallery.php
+++ _build/properties/properties.ms_gallery.php
@@ -3,76 +3,75 @@
 $properties = array();
 
 $tmp = array(
-	'product' => array(
-		'type' => 'numberfield',
-		'value' => '',
-	),
-	'tplRow' => array(
-		'type' => 'textfield',
-		'value' => 'tpl.msGallery.row',
-	),
-	'tplOuter' => array(
-		'type' => 'textfield',
-		'value' => 'tpl.msGallery.outer',
-	),
-	'tplEmpty' => array(
-		'type' => 'textfield',
-		'value' => 'tpl.msGallery.empty',
-	),
-	'tplSingle' => array(
-		'type' => 'textfield',
-		'value' => '',
-	),
-	'limit' => array(
-		'type' => 'numberfield',
-		'value' => 100,
-	),
-	'offset' => array(
-		'type' => 'numberfield',
-		'value' => 0,
-	),
-	'sortby' => array(
-		'type' => 'textfield',
-		'value' => 'rank',
-	),
-	'sortdir' => array(
-		'type' => 'list',
-		'options' => array(
-			array('text' => 'ASC','value' => 'ASC'),
-			array('text' => 'DESC','value' => 'DESC'),
-		),
-		'value' => 'ASC',
-	),
-	'toPlaceholder' => array(
-		'type' => 'textfield',
-		'value' => '',
-	),
-	'toSeparatePlaceholders' => array(
-		'type' => 'textfield',
-		'value' => '',
-	),
-	'showLog' => array(
-		'type' => 'combo-boolean',
-		'value' => false,
-	),
-	'where' => array(
-		'type' => 'textfield',
-		'value' => '',
-	),
-	'filetype' => array(
-		'type' => 'textfield',
-		'value' => '',
-		'desc' => 'ms2_prop_filetype',
-	),
+    'product' => array(
+        'type' => 'numberfield',
+        'value' => '',
+    ),
+    'tplRow' => array(
+        'type' => 'textfield',
+        'value' => 'tpl.msGallery.row',
+    ),
+    'tplOuter' => array(
+        'type' => 'textfield',
+        'value' => 'tpl.msGallery.outer',
+    ),
+    'tplEmpty' => array(
+        'type' => 'textfield',
+        'value' => 'tpl.msGallery.empty',
+    ),
+    'tplSingle' => array(
+        'type' => 'textfield',
+        'value' => '',
+    ),
+    'limit' => array(
+        'type' => 'numberfield',
+        'value' => 100,
+    ),
+    'offset' => array(
+        'type' => 'numberfield',
+        'value' => 0,
+    ),
+    'sortby' => array(
+        'type' => 'textfield',
+        'value' => 'rank',
+    ),
+    'sortdir' => array(
+        'type' => 'list',
+        'options' => array(
+            array('text' => 'ASC', 'value' => 'ASC'),
+            array('text' => 'DESC', 'value' => 'DESC'),
+        ),
+        'value' => 'ASC',
+    ),
+    'toPlaceholder' => array(
+        'type' => 'textfield',
+        'value' => '',
+    ),
+    'toSeparatePlaceholders' => array(
+        'type' => 'textfield',
+        'value' => '',
+    ),
+    'showLog' => array(
+        'type' => 'combo-boolean',
+        'value' => false,
+    ),
+    'where' => array(
+        'type' => 'textfield',
+        'value' => '',
+    ),
+    'filetype' => array(
+        'type' => 'textfield',
+        'value' => '',
+        'desc' => 'ms2_prop_filetype',
+    ),
 );
 
 foreach ($tmp as $k => $v) {
-	$properties[] = array_merge(array(
-			'name' => $k,
-			'desc' => 'ms2_prop_' . $k,
-			'lexicon' => 'minishop2:properties',
-		), $v
-	);
+    $properties[] = array_merge(array(
+        'name' => $k,
+        'desc' => 'ms2_prop_' . $k,
+        'lexicon' => 'minishop2:properties',
+    ), $v);
 }
 
 return $properties;
--- _build/setup.options.php
+++ _build/setup.options.php
@@ -1,71 +1,70 @@
 <?php
-/**
- * Build the setup options form.
- */
+
 $exists = $chunks = false;
 $output = null;
+/** @var array $options */
 switch ($options[xPDOTransport::PACKAGE_ACTION]) {
-	case xPDOTransport::ACTION_INSTALL:
-		$exists = $modx->getObject('transport.modTransportPackage', array('package_name' => 'pdoTools'));
-		break;
+    case xPDOTransport::ACTION_INSTALL:
+        $exists = $modx->getObject('transport.modTransportPackage', array('package_name' => 'pdoTools'));
+        break;
 
-	case xPDOTransport::ACTION_UPGRADE:
-		$exists = $modx->getObject('transport.modTransportPackage', array('package_name' => 'pdoTools'));
+    case xPDOTransport::ACTION_UPGRADE:
+        $exists = $modx->getObject('transport.modTransportPackage', array('package_name' => 'pdoTools'));
 
-		if (!empty($options['attributes']['chunks'])) {
-			$chunks = '<ul id="formCheckboxes" style="height:200px;overflow:auto;">';
-			foreach ($options['attributes']['chunks'] as $k => $v) {
-				$chunks .= '
+        if (!empty($options['attributes']['chunks'])) {
+            $chunks = '<ul id="formCheckboxes" style="height:200px;overflow:auto;">';
+            foreach ($options['attributes']['chunks'] as $k => $v) {
+                $chunks .= '
 				<li>
 					<label>
-						<input type="checkbox" name="update_chunks[]" value="'.$k.'"> '.$k.'
+						<input type="checkbox" name="update_chunks[]" value="' . $k . '"> ' . $k . '
 					</label>
 				</li>';
-			}
-			$chunks .= '</ul>';
-		}
+            }
+            $chunks .= '</ul>';
+        }
+        break;
 
-		break;
-
-	case xPDOTransport::ACTION_UNINSTALL: break;
+    case xPDOTransport::ACTION_UNINSTALL:
+        break;
 }
 
 $output = '';
 if (!$exists) {
-	switch ($modx->getOption('manager_language')) {
-		case 'ru':
-			$output = 'Этот компонент требует <b>pdoTools</b> для быстрой работы сниппетов.<br/>Он будет автоматически скачан и установлен.';
-			break;
-		default:
-			$output = 'This component requires <b>pdoTools</b> for fast work of snippets.<br/><br/>It will be automaticly downloaded and installed?';
-	}
+    switch ($modx->getOption('manager_language')) {
+        case 'ru':
+            $output = 'Этот компонент требует <b>pdoTools</b> для быстрой работы сниппетов.<br/>Он будет автоматически скачан и установлен.';
+            break;
+        default:
+            $output = 'This component requires <b>pdoTools</b> for fast work of snippets.<br/><br/>It will be automatically downloaded and installed?';
+    }
 
 }
 
 if ($chunks) {
-	if (!$exists) {
-		$output .= '<br/><br/>';
-	}
+    if (!$exists) {
+        $output .= '<br/><br/>';
+    }
 
-	switch ($modx->getOption('manager_language')) {
-		case 'ru':
-			$output .= 'Выберите чанки, которые нужно <b>перезаписать</b>:<br/>
+    switch ($modx->getOption('manager_language')) {
+        case 'ru':
+            $output .= 'Выберите чанки, которые нужно <b>перезаписать</b>:<br/>
 				<small>
 					<a href="#" onclick="Ext.get(\'formCheckboxes\').select(\'input\').each(function(v) {v.dom.checked = true;});">отметить все</a> |
 					<a href="#" onclick="Ext.get(\'formCheckboxes\').select(\'input\').each(function(v) {v.dom.checked = false;});">cнять отметки</a>
 				</small>
 			';
-			break;
-		default:
-			$output .= 'Select chunks, which need to <b>overwrite</b>:<br/>
+            break;
+        default:
+            $output .= 'Select chunks, which need to <b>overwrite</b>:<br/>
 				<small>
 					<a href="#" onclick="Ext.get(\'formCheckboxes\').select(\'input\').each(function(v) {v.dom.checked = true;});">select all</a> |
 					<a href="#" onclick="Ext.get(\'formCheckboxes\').select(\'input\').each(function(v) {v.dom.checked = false;});">deselect all</a>
 				</small>
 			';
-	}
+    }
 
-	$output .= $chunks;
+    $output .= $chunks;
 }
 
 return $output;
