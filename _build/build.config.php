c598ba76851dee5f7dc0afed760a317cef4500b4
Vasily Naumkin <bezumkin@yandex.ru>
2017-12-25 14:24:09 +0300

    Version 2.4.12-pl

--- _build/build.config.php
+++ _build/build.config.php
@@ -4,7 +4,7 @@
 define('PKG_NAME', 'miniShop2');
 define('PKG_NAME_LOWER', strtolower(PKG_NAME));
 
-define('PKG_VERSION', '2.4.11');
+define('PKG_VERSION', '2.4.12');
 define('PKG_RELEASE', 'pl');
 define('PKG_AUTO_INSTALL', true);
 
