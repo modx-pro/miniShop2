1d03c33fa180ed009fdd5b8021cf02d73c4ffc08
vgrish <vgrish@gmail.com>
2018-07-03 10:29:48 +0300

    - cosmetics fix

--- core/components/minishop2/model/minishop2/msproductdata.class.php
+++ core/components/minishop2/model/minishop2/msproductdata.class.php
@@ -562,7 +562,7 @@ class msProductData extends xPDOSimpleObject
                 $price = $params['price'] = $response['data']['price'];
             }
         }
-        
+
         return $price;
     }
 
