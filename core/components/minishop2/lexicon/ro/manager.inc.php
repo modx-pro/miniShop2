ce63ed6ca432c49c5999cdcfbfdfa3031356d76b
webinmd <89poilo@gmail.com>
2019-02-18 15:52:02 +0200

    Added romanian lexicon

---
+++ core/components/minishop2/lexicon/ro/manager.inc.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Manager Romanian Lexicon Entries for miniShop2
+ * translated by Anna
+ * 
+ * @package minishop2
+ * @subpackage lexicon
+ */
+$_lang['ms2_menu_create'] = 'A crea';
+$_lang['ms2_menu_copy'] = 'A copia';
+$_lang['ms2_menu_add'] = 'A adăuga';
+$_lang['ms2_menu_update'] = 'A modifica';
+$_lang['ms2_menu_remove'] = 'A șterge';
+$_lang['ms2_menu_remove_multiple'] = 'A șterge elementele selectate'; 
+$_lang['ms2_menu_remove_confirm'] = 'Sunteți sigur că doriți să ștergeți înregistrarea?'; 
+$_lang['ms2_menu_remove_multiple_confirm'] = 'Sunteți sigur că doriți să ștergeți toate înregistrările selectate'; 
+$_lang['ms2_menu_enable'] = 'A activa';
+$_lang['ms2_menu_disable'] = 'A dezactiva';
+$_lang['ms2_menu_select_all'] = 'A selecta totul';
+$_lang['ms2_menu_clear_all'] = 'A șterge toate';
+
+$_lang['ms2_combo_select'] = 'Apăsați pentru selectare';
+$_lang['ms2_combo_select_status'] = 'Filtrarea după statut';
+
+$_lang['ms2_id'] = 'Id';
+$_lang['ms2_key'] = 'Cheie';
+$_lang['ms2_name'] = 'Nume';
+$_lang['ms2_caption'] = 'Titlu';
+$_lang['ms2_color'] = 'Culoare';
+$_lang['ms2_country'] = 'Țară';
+$_lang['ms2_logo'] = 'Logou';
+$_lang['ms2_address'] = 'Adresă';
+$_lang['ms2_phone'] = 'Telefon';
+$_lang['ms2_fax'] = 'Fax';
+$_lang['ms2_email'] = 'Email';
+$_lang['ms2_active'] = 'Activ';
+$_lang['ms2_required'] = 'Obligatoriu';
+$_lang['ms2_class'] = 'Clasa handler';
+$_lang['ms2_description'] = 'Descriere';
+$_lang['ms2_num'] = 'Număr';
+$_lang['ms2_status'] = 'Statut';
+$_lang['ms2_count'] = 'Numărul';
+$_lang['ms2_cost'] = 'Preț';
+$_lang['ms2_order_cost'] = 'Prețul comenzii';
+$_lang['ms2_cart_cost'] = 'Prețul cumpărăturilor';
+$_lang['ms2_delivery_cost'] = 'Prețul livrării';
+$_lang['ms2_weight'] = 'Greutate';
+$_lang['ms2_createdon'] = 'Data creării';
+$_lang['ms2_updatedon'] = 'Data modificării';
+$_lang['ms2_user'] = 'Utilizator';
+$_lang['ms2_timestamp'] = 'Timestamp';
+$_lang['ms2_order_log'] = 'Istoricul comenzii';
+$_lang['ms2_order_products'] = 'Cumpărături';
+$_lang['ms2_action'] = 'Acțiune';
+$_lang['ms2_entry'] = 'Înregistrare';
+$_lang['ms2_username'] = 'Login';
+$_lang['ms2_fullname'] = 'Utilizatorul';
+$_lang['ms2_resource'] = 'Resursă';
+
+$_lang['ms2_receiver'] = 'Destinatar';
+$_lang['ms2_index'] = 'Index';
+$_lang['ms2_region'] = 'Regiune';
+$_lang['ms2_city'] = 'Oraș';
+$_lang['ms2_metro'] = 'Stație de metrou';
+$_lang['ms2_street'] = 'Stradă';
+$_lang['ms2_building'] = 'Clădire';
+$_lang['ms2_room'] = 'Camera';
+$_lang['ms2_comment'] = 'Comentariu';
+
+$_lang['ms2_email_user'] = 'Scrisoare cumpărătorului';
+$_lang['ms2_email_manager'] = 'Scrisoare managerului';
+$_lang['ms2_subject_user'] = 'Tema scrisorii cumpărătorului'; 
+$_lang['ms2_subject_manager'] = 'Tema scrisorii managerului'; 
+$_lang['ms2_body_user'] = 'Chunk scrisorii cumpărătorului'; 
+$_lang['ms2_body_manager'] = 'Chunk scrisorii managerului';
+$_lang['ms2_status_final'] = 'Final';
+$_lang['ms2_status_final_help'] = 'Dacă statut este final - nu poate fi schimbat cu altul.'; 
+$_lang['ms2_status_fixed'] = 'Fixează';
+$_lang['ms2_status_fixed_help'] = 'Statut de fixare interzice trecerea la statuturi, care merg în tabel înainte de el.';  
+$_lang['ms2_options'] = 'Opțiuni';
+$_lang['ms2_add_cost'] = 'Preț suplimentar';
+$_lang['ms2_add_cost_help'] = 'Preț suplimentar al livrării sau achitării. Poate fi negativ, puteți afișa procentele.'; 
+$_lang['ms2_weight_price'] = 'Preț pe unitate/ greutate'; 
+$_lang['ms2_weight_price_help'] = 'Preț suplimentar a livrării pe unitate de greutate. <br/> Poate fi folosit în clase personalizate.'; 
+$_lang['ms2_distance_price'] = 'Preț pentru unitate';
+$_lang['ms2_distance_price_help'] = 'Preț suplimentar a livrării pe unitate de distanță. Poate fi utilizat în clase personalizate.';
+$_lang['ms2_order_requires'] = 'Câmpuri obligatorii';
+$_lang['ms2_order_requires_help'] = 'La efectuarea comenzii, clasa personalizată poate să necesite completarea acestor câmpuri.'; 
+$_lang['ms2_rank'] = 'Ordinea';
+
+$_lang['ms2_orders_selected_status'] = 'A schimba statutul';
+
+$_lang['ms2_link_name'] = 'Denumirea link-ului';
+$_lang['ms2_link_one_to_one'] = 'Unu la altu';
+$_lang['ms2_link_one_to_one_desc'] = 'Conexiunea egală a două produse. Dacă doriți să conectați mai mult de 2 produse, trebuie să utilizați "mulți la mulți".'; 
+$_lang['ms2_link_one_to_many'] = 'Unu la mulți';
+$_lang['ms2_link_one_to_many_desc'] = 'Conexiunea produsului principal cu cele subordonate. De exemplu, produsul prezintă un set din alte produse. Se va potrivi bine pentru indicarea produselor recomandate.';
+$_lang['ms2_link_many_to_one'] = 'Mulți la unu';
+$_lang['ms2_link_many_to_one_desc'] = 'Conexiunea produsului principal cu cele subordonate. De exemplu, produsul prezintă un set de alte produse. Se va potrivi bine pentru produse recomandate.';
+$_lang['ms2_link_many_to_many'] = 'Mulți la mulți';
+$_lang['ms2_link_many_to_many_desc'] = 'Conexiunea egală a multor produse. Toate produsele din grup sunt conectate unele cu altele și atunci când o nouă conexiune este adăugată la una dintre ele, altele deasemenea o vor primi. Aplicație tipică: conexiune prin anumite parametri, de exemplu, culoare, dimensiune, versiune lingvistică etc.';
+$_lang['ms2_link_master'] = 'Produs principal'; 
+$_lang['ms2_link_slave'] = 'Produs slave'; 
+
+$_lang['ms2_ft_active'] = 'Activată';
+$_lang['ms2_ft_caption'] = 'Denumire';
+$_lang['ms2_ft_description'] = 'Descriere';
+$_lang['ms2_ft_measure_unit'] = 'Unitate de măsură';
+$_lang['ms2_ft_group'] = 'Grupă';
+$_lang['ms2_ft_groups'] = 'Grupuri';
+$_lang['ms2_ft_nogroup'] = 'Fără grup';
+$_lang['ms2_ft_name'] = 'Cheie';
+$_lang['ms2_ft_required'] = 'Obligatorie';
+$_lang['ms2_ft_type'] = 'Tipul caracteristicii';
+$_lang['ms2_ft_rank'] = 'Ordinea sortării';
+
+$_lang['ms2_ft_selected_delete'] = 'A șterge';
+$_lang['ms2_ft_selected_activate'] = 'A activa';
+$_lang['ms2_ft_selected_deactivate'] = 'A deactiva';
+$_lang['ms2_ft_selected_require'] = 'A o face obligatorie';
+$_lang['ms2_ft_selected_unrequire'] = 'A o face neobligatorie';
+$_lang['ms2_ft_selected_assign'] = 'A atribui categoria';
+$_lang['ms2_options_remove_confirm'] = 'Sunteți sigur că doriți să ștergeți toate opțiunile selectate? Valorile acestor opțiuni în produse vor fi șterse fără posibilitatea de recuperare.'; 
+$_lang['ms2_category_options_assign'] = 'Atribuiți opțiunile categoriii.';
+
+$_lang['ms2_ft_textfield'] = 'Câmpul text';
+$_lang['ms2_ft_numberfield'] = 'Câmp numeric'; 
+$_lang['ms2_ft_textarea'] = 'Zona de text'; 
+$_lang['ms2_ft_combobox'] = 'Listă derulantă';
+$_lang['ms2_ft_combo-boolean'] = 'Da/Nu';
+$_lang['ms2_ft_combo-multiple'] = 'Listă multiplă';
+$_lang['ms2_ft_combo-options'] = ' Listă de sugestii de auto-completare';
+$_lang['ms2_ft_checkbox'] = 'Caseta de selectare';
+$_lang['ms2_ft_datefield'] = 'Data';
+
+$_lang['ms2_orders_form_begin'] = 'A selecta comenzile de la';
+$_lang['ms2_orders_form_end'] = 'A selecta comenzi după';
+$_lang['ms2_orders_form_status'] = 'Filtru după statut';
+$_lang['ms2_orders_form_search'] = 'Căutare (număr, email, comentariu)';
+$_lang['ms2_orders_form_customer'] = 'Filtru după client';
+$_lang['ms2_orders_form_context'] = 'Filtru după context';
+$_lang['ms2_orders_form_selected_num'] = 'Au fost selectate comenzi';
+$_lang['ms2_orders_form_selected_sum'] = 'în sumă, RUB.';
+$_lang['ms2_orders_form_month_num'] = 'Executat în 30 zile';
+$_lang['ms2_orders_form_month_sum'] = 'în sumă, RUB.';
+$_lang['ms2_orders_form_submit'] = 'A expedia';
+$_lang['ms2_orders_form_reset'] = 'A reseta';
+
+$_lang['ms2_tab_category'] = 'Categorie';
+$_lang['ms2_tab_products'] = 'Produse';
+$_lang['ms2_tab_options'] = 'Proprietățile produselor';
+$_lang['ms2_tab_comments'] = 'Comentarii';
+$_lang['ms2_tab_product'] = 'Produs';
+$_lang['ms2_tab_product_data'] = 'Proprietățile produsului';
+$_lang['ms2_tab_product_options'] = 'Obțiunile produsului';
+$_lang['ms2_tab_product_links'] = 'Link-uri';
+$_lang['ms2_tab_product_categories'] = 'Categorii';
+$_lang['ms2_tab_product_gallery'] = 'Galerie';
---
+++ core/components/minishop2/lexicon/ro/permissions.inc.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Permissions Romanian Lexicon Entries for miniShop2
+ * translated by Anna
+ *
+ *
+ * @package minishop2
+ * @subpackage lexicon
+ */
+$_lang['mscategory_save'] = 'Permite crearea \ modificarea categoriei magazinului';
+$_lang['msproduct_save'] = 'Permite crearea\ modificarea produsului magazinului '; 
+
+$_lang['msorder_view'] = 'Permite vizualizarea comenzii '; 
+$_lang['msorder_list'] = 'Permite afișarea listei de comenzi'; 
+$_lang['msorder_save'] = 'Permite modificarea comenzii';
+
+$_lang['mssetting_view'] = 'Permite vizualizarea setărilor'; 
+$_lang['mssetting_list'] = 'Permite afișarea unei liste de setări';
+$_lang['mssetting_save'] = 'Permite crearea\ modificarea setărilor'; 
+
+$_lang['msproductfile_save'] = 'Permite crearea\ modificarea fișierelor produsului'; 
+$_lang['msproductfile_generate'] = 'Permite generarea miniaturilor a produsului'; 
+$_lang['msproductfile_list'] = 'Permite afișarea unei liste de fișiere a produsului.'; 
