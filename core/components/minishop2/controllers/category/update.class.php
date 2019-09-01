b47f4c2573c460c4e412a565c7989fcd93ad4915
Bakhtovar Gulomov <gulomovcreative@gmail.com>
2019-09-01 13:03:01 +0700

    Fix showing IDs on modx-resource-header

--- core/components/minishop2/controllers/category/update.class.php
+++ core/components/minishop2/controllers/category/update.class.php
@@ -134,6 +134,7 @@ class msCategoryUpdateManagerController extends msResourceUpdateController
             Ext.onReady(function() {
                 MODx.load(' . json_encode($ready) . ');
             });
+            MODx.perm.tree_show_resource_ids = '.($this->modx->hasPermission('tree_show_resource_ids') ? 1 : 0).';
         // ]]>
         </script>');
 
