f5edc9943862be4a8e9b26debb3497cef2d319de
Bochkarev Ivan <ivanx86@gmail.com>
2020-11-11 15:40:07 +0600

    Fixed sorting by customer field in the order list

--- core/components/minishop2/processors/mgr/orders/getlist.class.php
+++ core/components/minishop2/processors/mgr/orders/getlist.class.php
@@ -110,7 +110,7 @@ class msOrderGetListProcessor extends modObjectGetListProcessor
         $start = (int)$this->getProperty('start');
 
         $q = clone $c;
-        $q->query['columns'] = ['SQL_CALC_FOUND_ROWS msOrder.id'];
+        $q->query['columns'] = ['SQL_CALC_FOUND_ROWS msOrder.id, fullname as customer'];
         $sortClassKey = $this->getSortClassKey();
         $sortKey = $this->modx->getSelectColumns($sortClassKey, $this->getProperty('sortAlias', $sortClassKey), '', [$this->getProperty('sort')]);
         if (empty($sortKey)) {
