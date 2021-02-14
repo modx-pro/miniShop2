23b0dec4019f5df715bcdbf45390f0285dc84f04
Nikolay Savin <info@megawebs.kz>
2021-02-14 18:08:38 +0600

    fix error with Recoverable error

--- core/components/minishop2/model/minishop2/msproductdata.class.php
+++ core/components/minishop2/model/minishop2/msproductdata.class.php
@@ -645,7 +645,7 @@ class msProductData extends xPDOSimpleObject
         );
         $response = $miniShop2->invokeEvent('msOnGetProductFields', $params);
         if ($response['success']) {
-            $data = array_merge($data, $response['data']);
+            $data = array_merge($data, $response['data']['data']);
         }
 
         return $data;
