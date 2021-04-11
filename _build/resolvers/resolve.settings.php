4a50a7f81fd6ac948b881f93eff399e2fb86158a
Bochkarev Ivan <ivanx86@gmail.com>
2021-04-11 17:28:51 +0600

    Fix code style

--- _build/resolvers/resolve.settings.php
+++ _build/resolvers/resolve.settings.php
@@ -9,10 +9,10 @@ if ($transport->xpdo) {
         case xPDOTransport::ACTION_INSTALL:
         case xPDOTransport::ACTION_UPGRADE:
             $modelPath = $modx->getOption(
-                    'minishop2.core_path',
-                    null,
-                    $modx->getOption('core_path') . 'components/minishop2/'
-                ) . 'model/';
+                'minishop2.core_path',
+                null,
+                $modx->getOption('core_path') . 'components/minishop2/'
+            ) . 'model/';
             $modx->addPackage('minishop2', $modelPath);
             $lang = $modx->getOption('manager_language') == 'en' ? 1 : 0;
 
