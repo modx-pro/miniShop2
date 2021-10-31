dd77ee4e0df0bf43bfd35cc0d3fc019f8e48ea44
Nikolay Savin <info@megawebs.kz>
2021-10-31 13:54:01 +0600

    добавил free_delivery_amount в прослушку цен

--- core/components/minishop2/processors/mgr/settings/delivery/create.class.php
+++ core/components/minishop2/processors/mgr/settings/delivery/create.class.php
@@ -39,12 +39,10 @@ class msDeliveryCreateProcessor extends modObjectCreateProcessor
             $this->modx->error->addField('name', $this->modx->lexicon('ms2_err_ae'));
         }
 
-        $prices = array('price', 'distance_price', 'weight_price');
+        $prices = array('price', 'distance_price', 'weight_price', 'free_delivery_amount');
         foreach ($prices as $field) {
-            if ($tmp = $this->getProperty($field)) {
-                $tmp = $this->preparePrice($tmp);
-                $this->setProperty($field, $tmp);
-            }
+            $tmp = $this->preparePrice($tmp);
+            $this->setProperty($field, $tmp);
         }
 
         return !$this->hasErrors();
