3682e551d86ef29965f780899b76fe626450ca59
Nikolay Savin <info@megawebs.kz>
2022-09-15 23:36:11 +0600

    интерфейсы контроллеров вынесены в отдельные файлы

--- core/components/minishop2/handlers/msdeliveryhandler.class.php
+++ core/components/minishop2/handlers/msdeliveryhandler.class.php
@@ -1,44 +1,6 @@
 <?php
 
-interface msDeliveryInterface
-{
-
-    /**
-    * Returns an additional cost depending on the method of delivery
-    *
-    * @param msOrderInterface $order
-    * @param msDelivery $delivery
-    * @param float $cost
-    *
-    * @return float|integer
-    */
-    public function getCost(msOrderInterface $order, msDelivery $delivery, $cost = 0.0);
-
-
-    /**
-    * Returns failure response
-    *
-    * @param string $message
-    * @param array $data
-    * @param array $placeholders
-    *
-    * @return array|string
-    */
-    public function error($message = '', $data = array(), $placeholders = array());
-
-
-    /**
-    * Returns success response
-    *
-    * @param string $message
-    * @param array $data
-    * @param array $placeholders
-    *
-    * @return array|string
-    */
-    public function success($message = '', $data = array(), $placeholders = array());
-}
-
+require_once dirname(__FILE__) . '/interfaces/msDeliveryInterface.php';
 
 class msDeliveryHandler implements msDeliveryInterface
 {
--- core/components/minishop2/handlers/mspaymenthandler.class.php
+++ core/components/minishop2/handlers/mspaymenthandler.class.php
@@ -1,64 +1,6 @@
 <?php
 
-interface msPaymentInterface
-{
-
-    /**
-    * Send user to payment service
-    *
-    * @param msOrder $order Object with an order
-    *
-    * @return array|boolean $response
-    */
-    public function send(msOrder $order);
-
-
-    /**
-    * Receives payment
-    *
-    * @param msOrder $order Object with an order
-    *
-    * @return array|boolean $response
-    */
-    public function receive(msOrder $order);
-
-
-    /**
-    * Returns an additional cost depending on the method of payment
-    *
-    * @param msOrderInterface $order
-    * @param msPayment $payment
-    * @param float $cost
-    *
-    * @return integer
-    */
-    public function getCost(msOrderInterface $order, msPayment $payment, $cost = 0.0);
-
-
-    /**
-    * Returns failure response
-    *
-    * @param string $message
-    * @param array $data
-    * @param array $placeholders
-    *
-    * @return array|string
-    */
-    public function error($message = '', $data = array(), $placeholders = array());
-
-
-    /**
-    * Returns success response
-    *
-    * @param string $message
-    * @param array $data
-    * @param array $placeholders
-    *
-    * @return array|string
-    */
-    public function success($message = '', $data = array(), $placeholders = array());
-}
-
+require_once dirname(__FILE__) . '/interfaces/msPaymentInterface.php';
 
 class msPaymentHandler implements msPaymentInterface
 {
