e8434c25dd6c7498f6b8824ea14bbdab53013b93
ShevArtV <shev.art.v@ya.ru>
2022-09-27 22:51:15 +0300

    Переписал метод setHandler()

--- assets/components/minishop2/js/web/vanilajs/modules/minishop.class.js
+++ assets/components/minishop2/js/web/vanilajs/modules/minishop.class.js
@@ -1,9 +1,19 @@
 export default class MiniShop {
     constructor(config) {
-        this.config = Object.assign(config, {
+        const defaults = {
+            notifyClassPath: './msnotify.class.js',
+            notifyClassName: 'MsNotify',
+            cartClassPath: './mscart.class.js',
+            cartClassName: 'MsCart',
+            orderClassPath: './msorder.class.js',
+            orderClassName: 'MsOrder',
+            moduleImportErrorMsg: 'Произошла ошибка при загрузке модуля',
+            properties: ['Message', 'Cart', 'Order'],
             actionUrl: document.location.href,
             formMethod: 'POST',
-        });
+        };
+        this.config = Object.assign(defaults, config);
+
         this.config.callbacksObjectTemplate = this.callbacksObjectTemplate;
         this.Callbacks = this.config.Callbacks = {
             Cart: {
@@ -32,54 +42,35 @@ export default class MiniShop {
         this.initialize();
     }
 
-    async setHandler(property, pathPropertyName, classnamePropertyName, defaultPath, defaultClassName, errorMsg, response) {
-        const classPath = (this.config.hasOwnProperty(pathPropertyName) && this.config[pathPropertyName]) ?
-                this.config[pathPropertyName] : defaultPath,
-            className = (this.config.hasOwnProperty(classnamePropertyName) && this.config[classnamePropertyName]) ?
-                this.config[classnamePropertyName] : defaultClassName,
-            config = response ? response[className] : this;
+    async setHandler(property){
+        let prefix = property.toLowerCase(),
+            response = false,
+            messageSettings = false;
+        if(prefix === 'message'){
+            prefix = 'notify';
+            response = await this.sendResponse({url: this.config.notifySettingsPath, method: 'GET'});
+            if (response.ok) {
+                messageSettings = await response.json();
+            }
+        }
+        const classPath = this.config[prefix + 'ClassPath'];
+        const className = this.config[prefix + 'ClassName'];
+        const config = messageSettings ? messageSettings[className] : this;
 
         try {
-            const { default: ModuleName } = await import(classPath);
+            const {default: ModuleName} = await import(classPath);
             this[property] = new ModuleName(config);
         } catch (e) {
-            console.error(e, errorMsg);
+            throw new Error(this.config.moduleImportErrorMsg);
         }
     }
 
     async initialize() {
-        if (this.config.notifySettingsPath) {
-            const response = await this.sendResponse({ url: this.config.notifySettingsPath, method: 'GET' });
-            if (response.ok) {
-                const messageSettings = await response.json();
-                if (messageSettings) {
-                    this.setHandler(
-                        'Message',
-                        'notifyClassPath',
-                        'notifyClassName',
-                        './msnotify.class.js',
-                        'MsNotify',
-                        'Произошла ошибка при загрузке модуля уведомлений',
-                        messageSettings);
-                }
-            }
-        }
+        if(!this.config.properties.length) { throw new Error('Не передан массив имён обработчиков'); }
 
-        this.setHandler(
-            'Cart',
-            'cartClassPath',
-            'cartClassName',
-            './mscart.class.js',
-            'MsCart',
-            'Произошла ошибка при загрузке модуля корзины');
-
-        this.setHandler(
-            'Order',
-            'orderClassPath',
-            'orderClassName',
-            './msorder.class.js',
-            'MsOrder',
-            'Произошла ошибка при загрузке модуля отправки заказа');
+        await this.config.properties.forEach(property => {
+            this.setHandler(property);
+        });
 
         document.addEventListener('submit', e => {
             e.preventDefault();
