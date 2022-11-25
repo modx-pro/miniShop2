5dcbe7d5ac2c802e91a1059763d9a8fa4e03fe34
E Webinmd <webinmd@Es-iMac.local>
2022-11-25 23:31:23 +0200

    Fix error on ms2_options table create

--- core/components/minishop2/model/minishop2/mysql/msoption.map.inc.php
+++ core/components/minishop2/model/minishop2/mysql/msoption.map.inc.php
@@ -6,171 +6,171 @@ $xpdo_meta_map['msOption'] = array(
     'table' => 'ms2_options',
     'extends' => 'xPDOSimpleObject',
     'fields' =>
-        array(
-            'key' => '',
-            'caption' => '',
-            'description' => null,
-            'measure_unit' => null,
-            'category' => null,
-            'type' => '',
-            'properties' => null,
-        ),
+    array(
+        'key' => '',
+        'caption' => '',
+        'description' => null,
+        'measure_unit' => null,
+        'category' => null,
+        'type' => '',
+        'properties' => null,
+    ),
     'fieldMeta' =>
+    array(
+        'key' =>
         array(
-            'key' =>
-                array(
-                    'dbtype' => 'varchar',
-                    'precision' => '255',
-                    'phptype' => 'string',
-                    'null' => false,
-                    'default' => '',
-                    'index' => 'index',
-                ),
-            'caption' =>
-                array(
-                    'dbtype' => 'varchar',
-                    'precision' => '255',
-                    'phptype' => 'string',
-                    'null' => false,
-                    'default' => '',
-                    'index' => 'fulltext',
-                ),
-            'description' =>
-                array(
-                    'dbtype' => 'text',
-                    'phptype' => 'string',
-                    'null' => true,
-                ),
-            'measure_unit' =>
-                array(
-                    'dbtype' => 'tinytext',
-                    'phptype' => 'string',
-                    'null' => true,
-                ),
-            'category' =>
-                array(
-                    'dbtype' => 'int',
-                    'precision' => '10',
-                    'attributes' => 'unsigned',
-                    'phptype' => 'integer',
-                    'null' => false,
-                ),
-            'type' =>
-                array(
-                    'dbtype' => 'varchar',
-                    'precision' => '255',
-                    'phptype' => 'string',
-                    'null' => false,
-                    'default' => '',
-                    'index' => 'index',
-                ),
-            'properties' =>
-                array(
-                    'dbtype' => 'text',
-                    'phptype' => 'json',
-                    'null' => true,
-                ),
+            'dbtype' => 'varchar',
+            'precision' => '191',
+            'phptype' => 'string',
+            'null' => false,
+            'default' => '',
+            'index' => 'index',
         ),
+        'caption' =>
+        array(
+            'dbtype' => 'varchar',
+            'precision' => '191',
+            'phptype' => 'string',
+            'null' => false,
+            'default' => '',
+            'index' => 'fulltext',
+        ),
+        'description' =>
+        array(
+            'dbtype' => 'text',
+            'phptype' => 'string',
+            'null' => true,
+        ),
+        'measure_unit' =>
+        array(
+            'dbtype' => 'tinytext',
+            'phptype' => 'string',
+            'null' => true,
+        ),
+        'category' =>
+        array(
+            'dbtype' => 'int',
+            'precision' => '10',
+            'attributes' => 'unsigned',
+            'phptype' => 'integer',
+            'null' => false,
+        ),
+        'type' =>
+        array(
+            'dbtype' => 'varchar',
+            'precision' => '191',
+            'phptype' => 'string',
+            'null' => false,
+            'default' => '',
+            'index' => 'index',
+        ),
+        'properties' =>
+        array(
+            'dbtype' => 'text',
+            'phptype' => 'json',
+            'null' => true,
+        ),
+    ),
     'indexes' =>
+    array(
+        'key' =>
+        array(
+            'alias' => 'type',
+            'primary' => false,
+            'unique' => false,
+            'type' => 'BTREE',
+            'columns' =>
+            array(
+                'type' =>
+                array(
+                    'length' => '',
+                    'collation' => 'A',
+                    'null' => false,
+                ),
+            ),
+        ),
+        'caption_ft' =>
+        array(
+            'alias' => 'caption_ft',
+            'primary' => false,
+            'unique' => false,
+            'type' => 'FULLTEXT',
+            'columns' =>
+            array(
+                'caption' =>
+                array(
+                    'length' => '',
+                    'collation' => 'A',
+                    'null' => false,
+                ),
+            ),
+        ),
+        'category' =>
+        array(
+            'alias' => 'category',
+            'primary' => false,
+            'unique' => false,
+            'type' => 'BTREE',
+            'columns' =>
+            array(
+                'category' =>
+                array(
+                    'length' => '',
+                    'collation' => 'A',
+                    'null' => false,
+                ),
+            ),
+        ),
+    ),
+    'composites' =>
+    array(
+        'OptionCategories' =>
+        array(
+            'class' => 'msCategoryOption',
+            'local' => 'id',
+            'foreign' => 'option_id',
+            'cardinality' => 'many',
+            'owner' => 'local',
+        ),
+        'OptionProducts' =>
+        array(
+            'class' => 'msProductOption',
+            'local' => 'key',
+            'foreign' => 'key',
+            'cardinality' => 'many',
+            'owner' => 'local',
+        ),
+    ),
+    'aggregates' =>
+    array(
+        'Category' =>
+        array(
+            'class' => 'modCategory',
+            'local' => 'category',
+            'foreign' => 'id',
+            'owner' => 'foreign',
+            'cardinality' => 'one',
+        ),
+    ),
+    'validation' =>
+    array(
+        'rules' =>
         array(
             'key' =>
+            array(
+                'invalid' =>
                 array(
-                    'alias' => 'type',
-                    'primary' => false,
-                    'unique' => false,
-                    'type' => 'BTREE',
-                    'columns' =>
-                        array(
-                            'type' =>
-                                array(
-                                    'length' => '',
-                                    'collation' => 'A',
-                                    'null' => false,
-                                ),
-                        ),
+                    'type' => 'preg_match',
+                    'rule' => '/^(?!\\W+)(?!\\d)[a-zA-Z0-9\\x2d-\\x2f\\x7f-\\xff-_]+(?!\\s)$/',
+                    'message' => 'ms2_option_err_invalid_key',
                 ),
-            'caption_ft' =>
+                'reserved' =>
                 array(
-                    'alias' => 'caption_ft',
-                    'primary' => false,
-                    'unique' => false,
-                    'type' => 'FULLTEXT',
-                    'columns' =>
-                        array(
-                            'caption' =>
-                                array(
-                                    'length' => '',
-                                    'collation' => 'A',
-                                    'null' => false,
-                                ),
-                        ),
+                    'type' => 'preg_match',
+                    'rule' => '/^(?!(id|type|contentType|pagetitle|longtitle|description|alias|link_attributes|published|pub_date|unpub_date|parent|isfolder|introtext|content|richtext|template|menuindex|searchable|cacheable|createdby|createdon|editedby|editedon|deleted|deletedby|deletedon|publishedon|publishedby|menutitle|donthit|privateweb|privatemgr|content_dispo|hidemenu|class_key|context_key|content_type|uri|uri_override|hide_children_in_tree|show_in_tree|article|price|old_price|weight|image|thumb|vendor|made_in|new|popular|favorite|tags|color|size|source|action)$)/',
+                    'message' => 'ms2_option_err_reserved_key',
                 ),
-            'category' =>
-                array(
-                    'alias' => 'category',
-                    'primary' => false,
-                    'unique' => false,
-                    'type' => 'BTREE',
-                    'columns' =>
-                        array(
-                            'category' =>
-                                array(
-                                    'length' => '',
-                                    'collation' => 'A',
-                                    'null' => false,
-                                ),
-                        ),
-                ),
+            ),
         ),
-    'composites' =>
-        array(
-            'OptionCategories' =>
-                array(
-                    'class' => 'msCategoryOption',
-                    'local' => 'id',
-                    'foreign' => 'option_id',
-                    'cardinality' => 'many',
-                    'owner' => 'local',
-                ),
-            'OptionProducts' =>
-                array(
-                    'class' => 'msProductOption',
-                    'local' => 'key',
-                    'foreign' => 'key',
-                    'cardinality' => 'many',
-                    'owner' => 'local',
-                ),
-        ),
-    'aggregates' =>
-        array(
-            'Category' =>
-                array(
-                    'class' => 'modCategory',
-                    'local' => 'category',
-                    'foreign' => 'id',
-                    'owner' => 'foreign',
-                    'cardinality' => 'one',
-                ),
-        ),
-    'validation' =>
-        array(
-            'rules' =>
-                array(
-                    'key' =>
-                        array(
-                            'invalid' =>
-                                array(
-                                    'type' => 'preg_match',
-                                    'rule' => '/^(?!\\W+)(?!\\d)[a-zA-Z0-9\\x2d-\\x2f\\x7f-\\xff-_]+(?!\\s)$/',
-                                    'message' => 'ms2_option_err_invalid_key',
-                                ),
-                            'reserved' =>
-                                array(
-                                    'type' => 'preg_match',
-                                    'rule' => '/^(?!(id|type|contentType|pagetitle|longtitle|description|alias|link_attributes|published|pub_date|unpub_date|parent|isfolder|introtext|content|richtext|template|menuindex|searchable|cacheable|createdby|createdon|editedby|editedon|deleted|deletedby|deletedon|publishedon|publishedby|menutitle|donthit|privateweb|privatemgr|content_dispo|hidemenu|class_key|context_key|content_type|uri|uri_override|hide_children_in_tree|show_in_tree|article|price|old_price|weight|image|thumb|vendor|made_in|new|popular|favorite|tags|color|size|source|action)$)/',
-                                    'message' => 'ms2_option_err_reserved_key',
-                                ),
-                        ),
-                ),
-        ),
+    ),
 );
