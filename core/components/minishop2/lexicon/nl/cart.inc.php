5d212f0c5dcab678d0dd460d74a734d727694e99
Ruslan Aleev <aleev.insmile@gmail.com>
2023-01-19 20:53:51 +0400

    New translations cart.inc.php (Dutch)

--- core/components/minishop2/lexicon/nl/cart.inc.php
+++ core/components/minishop2/lexicon/nl/cart.inc.php
@@ -1,35 +1,43 @@
 <?php
 
 /**
- * Default English Lexicon Entries for miniShop2 cart
+ * Cart Lexicon Entries
  *
  * @package minishop2
  * @subpackage lexicon
  */
 
-$_lang['ms2_cart'] = 'Winkelwagen';
-$_lang['ms2_minicart'] = 'Winkelwagen';
-$_lang['ms2_minicart_goods'] = 'Totaal producten:';
-$_lang['ms2_minicart_cost'] = 'Totale kosten:';
-$_lang['ms2_minicart_is_empty'] = 'Je winkelwagen is leeg';
+$_lang['ms2_cart'] = 'Warenkorb';
+$_lang['ms2_minicart'] = 'Warenkorb';
+
+$_lang['ms2_minicart_goods'] = 'Alle Waren:';
+$_lang['ms2_minicart_cost'] = 'for the sum:';
+$_lang['ms2_minicart_is_empty'] = 'Ihr Warenkorb ist leer';
+
 $_lang['ms2_cart_title'] = 'Titel';
-$_lang['ms2_cart_price'] = 'Prijs';
+$_lang['ms2_cart_price'] = 'Preis';
 $_lang['ms2_cart_weight'] = 'Gewicht';
-$_lang['ms2_cart_count'] = 'Aantal';
-$_lang['ms2_cart_cost'] = 'Prijs';
-$_lang['ms2_cart_remove'] = 'Verwijderen';
-$_lang['ms2_cart_total'] = 'Totaal';
-$_lang['ms2_cart_add_success'] = 'Het product is toegevoegd aan de winkelwagen [[+count]]';
-$_lang['ms2_cart_add_error'] = 'Het toevoegen is niet gelukt';
-$_lang['ms2_cart_add_err_id'] = 'Fout bij het toevoegen: id van het product niet gevonden';
-$_lang['ms2_cart_add_err_nf'] = 'Fout bij het toevoegen: product niet gevonden';
-$_lang['ms2_cart_add_err_count'] = 'Fout bij het toevoegen: verkeerde telling "[[+count]]"';
-$_lang['ms2_cart_remove_success'] = 'Product is verwijderd uit winkelwagen';
-$_lang['ms2_cart_remove_error'] = 'Fout bij het verwijderen van het product uit de winkelwagen';
-$_lang['ms2_cart_change_success'] = 'Hoeveelheid succesvol aangepast: [[+count]]';
-$_lang['ms2_cart_change_error'] = 'Fout bij veranderen van hoeveelheid in winkelwagen';
-$_lang['ms2_cart_clean_success'] = 'Winkelwagen succesvol geleegd';
-$_lang['ms2_cart_is_empty'] = 'Winkelwagen is leeg';
-$_lang['ms2_cart_clean'] = 'Leeg winkelwagen';
-$_lang['ms2_order_err_delivery'] = 'Kies een verzendmethode';
-$_lang['ms2_order_err_requires'] = 'Vul de uitgelichte velden in';
+$_lang['ms2_cart_count'] = 'Anzahl';
+$_lang['ms2_cart_cost'] = 'Kosten';
+$_lang['ms2_cart_remove'] = 'Entfernen';
+$_lang['ms2_cart_total'] = 'Gesamt';
+
+$_lang['ms2_cart_add_success'] = 'Produkt wurde zum Warenkorb hinzugefügt: [[+count]]';
+$_lang['ms2_cart_add_error'] = 'Beim Hinzufügen zum Warenkorb ist ein Fehler aufgetreten';
+$_lang['ms2_cart_add_err_id'] = 'Beim Hinzufügen zum Warenkorb ist ein Fehler aufgetreten: Die ID des Produktes ist nicht vergeben.';
+$_lang['ms2_cart_add_err_nf'] = 'Beim Hinzufügen zum Warenkorb ist ein Fehler aufgetreten: Produkt wurde nicht gefunden.';
+$_lang['ms2_cart_add_err_count'] = 'Beim Hinzufügen zum Warenkorb ist ein Fehler aufgetreten: Falsche Anzahl "[[+count]]"';
+
+$_lang['ms2_cart_remove_success'] = 'Produkt wurde aus dem Warenkorb entfernt';
+$_lang['ms2_cart_remove_error'] = 'Beim Entfernen aus dem Warenkorb ist ein Fehler aufgetreten';
+
+$_lang['ms2_cart_change_success'] = 'Anzahl der Produkte im Warenkorb wurde erfolgreich geändert: [[+count]]';
+$_lang['ms2_cart_change_error'] = 'Beim Ändern der Produktanzahl ist ein Fehler aufgetreten';
+
+$_lang['ms2_cart_clean_success'] = 'Der Warenkorb wurde erfolgreich geleert';
+$_lang['ms2_cart_is_empty'] = 'Ihr Warenkorb ist leer';
+$_lang['ms2_cart_clean'] = 'Warenkorb leeren';
+
+$_lang['ms2_order_err_empty'] = 'Невозможно оформить пустой заказ';
+$_lang['ms2_order_err_delivery'] = 'Bitte wählen Sie eine Lieferungsmethode';
+$_lang['ms2_order_err_requires'] = 'Bitte füllen Sie die Pflichtfelder aus';
