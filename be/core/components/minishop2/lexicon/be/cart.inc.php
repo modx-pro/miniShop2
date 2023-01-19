fd460d6305e9b650ff5e6e4d348f318cb232d88a
Ruslan Aleev <aleev.insmile@gmail.com>
2023-01-19 19:23:16 +0400

    New translations cart.inc.php (Belarusian)

---
+++ be/core/components/minishop2/lexicon/be/cart.inc.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Default Russian Lexicon Entries for miniShop2 cart
+ *
+ * @package minishop2
+ * @subpackage lexicon
+ */
+
+$_lang['ms2_cart'] = 'Корзина';
+$_lang['ms2_minicart'] = 'Корзина';
+
+$_lang['ms2_minicart_goods'] = 'Всего товаров:';
+$_lang['ms2_minicart_cost'] = 'на сумму:';
+$_lang['ms2_minicart_is_empty'] = 'Вы еще ничего не выбрали';
+
+$_lang['ms2_cart_title'] = 'Наименование';
+$_lang['ms2_cart_price'] = 'Цена';
+$_lang['ms2_cart_weight'] = 'Вес';
+$_lang['ms2_cart_count'] = 'Количество';
+$_lang['ms2_cart_cost'] = 'Стоимость';
+$_lang['ms2_cart_remove'] = 'Удалить';
+$_lang['ms2_cart_total'] = 'Итого';
+
+$_lang['ms2_cart_add_success'] = 'Товар успешно добавлен в корзину: [[+count]]';
+$_lang['ms2_cart_add_error'] = 'Ошибка добавления товара в корзину';
+$_lang['ms2_cart_add_err_id'] = 'Ошибка добавления в корзину: не указан идентификатор товара';
+$_lang['ms2_cart_add_err_nf'] = 'Ошибка добавления в корзину: товар не найден';
+$_lang['ms2_cart_add_err_count'] = 'Ошибка добавления в корзину: неверное количество "[[+count]]"';
+
+$_lang['ms2_cart_remove_success'] = 'Товар успешно удалён из корзины';
+$_lang['ms2_cart_remove_error'] = 'Ошибка удаления товара';
+
+$_lang['ms2_cart_change_success'] = 'Количество товара в корзине успешно изменено: [[+count]]';
+$_lang['ms2_cart_change_error'] = 'Ошибка при изменении количества товара в корзине';
+
+$_lang['ms2_cart_clean_success'] = 'Корзина успешно очищена';
+$_lang['ms2_cart_is_empty'] = 'Ваша корзина пуста';
+$_lang['ms2_cart_clean'] = 'Очистить корзину';
+
+$_lang['ms2_order_err_empty'] = 'Невозможно оформить пустой заказ';
+$_lang['ms2_order_err_delivery'] = 'Вы должны выбрать способ доставки';
+$_lang['ms2_order_err_requires'] = 'Вы должны заполнить требуемые поля';
