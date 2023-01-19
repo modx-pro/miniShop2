44c239596e84be6640fc7f98a78bb06bffc0b8cd
Ruslan Aleev <aleev.insmile@gmail.com>
2023-01-19 20:54:06 +0400

    New translations permissions.inc.php (French)

--- core/components/minishop2/lexicon/fr/permissions.inc.php
+++ core/components/minishop2/lexicon/fr/permissions.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Permissions French Lexicon Entries for miniShop2
+ * Permissions Lexicon Entries
  *
  * @package minishop2
  * @subpackage lexicon
@@ -13,6 +13,7 @@ $_lang['msproduct_save'] = 'Autorisation d`enregistrer\mise à jour minishop pro
 $_lang['msorder_view'] = 'Permission de vue de l`ordre';
 $_lang['msorder_list'] = 'Permission de liste de commandes';
 $_lang['msorder_save'] = 'Autorisation de la mise à jour de l`ordre';
+$_lang['msorder_remove'] = 'Разрешает удаление заказа';
 
 $_lang['mssetting_view'] = 'Permission de vue de paramètres';
 $_lang['mssetting_list'] = 'Permission de liste de paramètres';
@@ -21,3 +22,6 @@ $_lang['mssetting_save'] = 'Autorisation d`enregistrer\paramètres de mise à jo
 $_lang['msproductfile_save'] = 'Autorisation d`enregistrer\mise à jour des fichiers du produit';
 $_lang['msproductfile_generate'] = 'Permission de créer des miniatures d`images de produits';
 $_lang['msproductfile_list'] = 'Permission de liste de fichiers du produit';
+
+$_lang['msproduct_delete'] = 'Разрешает удаление товара';
+$_lang['msproduct_publish'] = 'Разрешает публикацию товара';
