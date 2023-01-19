0bc0ebd266d60a70bb54b9ee1b8fd9c2892bc939
Ruslan Aleev <aleev.insmile@gmail.com>
2023-01-19 20:54:03 +0400

    New translations manager.inc.php (Lithuanian)

--- core/components/minishop2/lexicon/lt/manager.inc.php
+++ core/components/minishop2/lexicon/lt/manager.inc.php
@@ -1,25 +1,32 @@
 <?php
 
 /**
- * Manager English Lexicon Entries for miniShop2
+ * Manager Lexicon Entries
  *
  * @package minishop2
  * @subpackage lexicon
  */
 
 $_lang['ms2_menu_create'] = 'Kurti';
+$_lang['ms2_menu_copy'] = 'Копировать';
 $_lang['ms2_menu_add'] = 'Pridėti';
 $_lang['ms2_menu_update'] = 'Atnaujinti';
 $_lang['ms2_menu_remove'] = 'Šalinti';
 $_lang['ms2_menu_remove_multiple'] = 'Šalinti pasirinktus';
 $_lang['ms2_menu_remove_confirm'] = 'Ar tikrai norite pašalinti šį įrašą?';
 $_lang['ms2_menu_remove_multiple_confirm'] = 'Ar tikrai norite pašalinti visus pasirinktus įrašus?';
+$_lang['ms2_menu_enable'] = 'Включить';
+$_lang['ms2_menu_disable'] = 'Выключить';
+$_lang['ms2_menu_select_all'] = 'Выделить всё';
+$_lang['ms2_menu_clear_all'] = 'Очистить всё';
 
 $_lang['ms2_combo_select'] = 'Spustelėkite pasirinkimui';
 $_lang['ms2_combo_select_status'] = 'Filtruoti pagal būseną';
 
 $_lang['ms2_id'] = 'Id';
+$_lang['ms2_key'] = 'Ключ';
 $_lang['ms2_name'] = 'Pavadinimas';
+$_lang['ms2_caption'] = 'Заголовок';
 $_lang['ms2_color'] = 'Spalva';
 $_lang['ms2_country'] = 'Valstybė';
 $_lang['ms2_logo'] = 'Logotipas';
@@ -28,6 +35,7 @@ $_lang['ms2_phone'] = 'Telefonas';
 $_lang['ms2_fax'] = 'Faksas';
 $_lang['ms2_email'] = 'El. paštas';
 $_lang['ms2_active'] = 'Aktyvus';
+$_lang['ms2_required'] = 'Обязательный';
 $_lang['ms2_class'] = 'Atsakinga klasė';
 $_lang['ms2_description'] = 'Aprašas';
 $_lang['ms2_num'] = 'Skaičius';
@@ -58,7 +66,11 @@ $_lang['ms2_metro'] = 'Metro stotelė';
 $_lang['ms2_street'] = 'Gatvė';
 $_lang['ms2_building'] = 'Namas';
 $_lang['ms2_room'] = 'Butas';
+$_lang['ms2_entrance'] = 'Подъезд';
+$_lang['ms2_floor'] = 'Этаж';
+$_lang['ms2_text_address'] = 'Адрес одной строкой';
 $_lang['ms2_comment'] = 'Komentaras';
+$_lang['ms2_order_comment'] = 'Комментарий оператора';
 
 $_lang['ms2_email_user'] = 'Siųsti laišką pirkėjui';
 $_lang['ms2_email_manager'] = 'Siųsti laišką vadybininkui';
@@ -79,10 +91,11 @@ $_lang['ms2_distance_price'] = 'Įkainis už atstumo vienetą';
 $_lang['ms2_distance_price_help'] = 'Papildomas įkainis už vieną atstumo vienetą.<br/>Gali būti naudojamas nuosavose klasėse.';
 $_lang['ms2_order_requires'] = 'Privalomi laukai';
 $_lang['ms2_order_requires_help'] = 'Užsakymo metu nuosavoje klasėje galima reikalauti užpildyti šiuos laukus';
-$_lang['ms2_orders_selected_status'] = 'Keisti pasirinktų užsakymų būseną';
+$_lang['ms2_rank'] = 'Порядок';
 $_lang['ms2_free_delivery_amount'] = 'Nemokamas pristatymas pagal užsakymo sumą';
 $_lang['ms2_free_delivery_amount_help'] = 'Kai užsakymo suma pasieks šią vertę, pristatymas bus nemokamas. Jei siuntimo klasė buvo pakeista ir (arba) jūs įdiegėte komponentus, kurie gali turėti įtakos užsakymo vertei, į šį lauką gali būti neatsižvelgta.';
-$_lang['ms2_orders_create_order'] = 'Sukurti užsakymą';
+
+$_lang['ms2_orders_selected_status'] = 'Keisti pasirinktų užsakymų būseną';
 
 $_lang['ms2_link_name'] = 'Sąryšio pavadinimas';
 $_lang['ms2_link_one_to_one'] = 'Viena su viena';
@@ -95,3 +108,59 @@ $_lang['ms2_link_many_to_many'] = 'Daug su daug';
 $_lang['ms2_link_many_to_many_desc'] = 'Daugiau nei dviejų lygiaverčių prekių susiejimas. Visos prekės susiejamos tarpusavyje ir susiejus bent vieną jų su nauja preke, bus automatiškai susiejamos ir kitos. Tipiniai panaudojimo būdai: vieno modelio skirtingų spalvų, dydžių, kalbų, versijų ir pan. prekėms susieti.';
 $_lang['ms2_link_master'] = 'Pagrindinė prekė';
 $_lang['ms2_link_slave'] = 'Antrinė prekė';
+
+$_lang['ms2_ft_active'] = 'Включена';
+$_lang['ms2_ft_caption'] = 'Название';
+$_lang['ms2_ft_description'] = 'Описание';
+$_lang['ms2_ft_measure_unit'] = 'Единица измерения';
+$_lang['ms2_ft_group'] = 'Группа';
+$_lang['ms2_ft_groups'] = 'Группы';
+$_lang['ms2_ft_nogroup'] = 'Без группы';
+$_lang['ms2_ft_name'] = 'Ключ';
+$_lang['ms2_ft_required'] = 'Обязательна';
+$_lang['ms2_ft_type'] = 'Тип характеристики';
+$_lang['ms2_ft_rank'] = 'Порядок сортировки';
+
+$_lang['ms2_ft_selected_delete'] = 'Убрать';
+$_lang['ms2_ft_selected_activate'] = 'Включить';
+$_lang['ms2_ft_selected_deactivate'] = 'Выключить';
+$_lang['ms2_ft_selected_require'] = 'Сделать обязательной';
+$_lang['ms2_ft_selected_unrequire'] = 'Сделать необязательной';
+$_lang['ms2_ft_selected_assign'] = 'Назначить в категорию';
+$_lang['ms2_options_remove_confirm'] = 'Вы уверены, что хотите удалить все выбранные опции? Значения этих опций в товарах будут удалены без возможности восстановления.';
+$_lang['ms2_category_options_assign'] = 'Назначить опции в категории.';
+
+$_lang['ms2_ft_textfield'] = 'Текстовое поле';
+$_lang['ms2_ft_numberfield'] = 'Числовое поле';
+$_lang['ms2_ft_textarea'] = 'Текстовая область';
+$_lang['ms2_ft_combobox'] = 'Выпадающий список';
+$_lang['ms2_ft_combo-boolean'] = 'Да/Нет';
+$_lang['ms2_ft_combo-multiple'] = 'Множественный список';
+$_lang['ms2_ft_combo-options'] = 'Список с автодополнением';
+$_lang['ms2_ft_checkbox'] = 'Флажок';
+$_lang['ms2_ft_datefield'] = 'Дата';
+
+$_lang['ms2_orders_form_begin'] = 'Выбрать заказы с';
+$_lang['ms2_orders_form_end'] = 'Выбрать заказы по';
+$_lang['ms2_orders_form_status'] = 'Фильтр по статусу';
+$_lang['ms2_orders_form_search'] = 'Поиск (номер, телефон, email, комментарий)';
+$_lang['ms2_orders_form_customer'] = 'Фильтр по заказчику';
+$_lang['ms2_orders_form_context'] = 'Фильтр по контексту';
+$_lang['ms2_orders_form_selected_num'] = 'Выбрано заказов';
+$_lang['ms2_orders_form_selected_sum'] = 'на сумму, руб.';
+$_lang['ms2_orders_form_month_num'] = 'Выполнено за 30 дней';
+$_lang['ms2_orders_form_month_sum'] = 'на сумму, руб.';
+$_lang['ms2_orders_form_submit'] = 'Отправить';
+$_lang['ms2_orders_form_reset'] = 'Сброс';
+$_lang['ms2_orders_create_order'] = 'Sukurti užsakymą';
+
+$_lang['ms2_tab_category'] = 'Категория';
+$_lang['ms2_tab_products'] = 'Товары';
+$_lang['ms2_tab_options'] = 'Свойства товаров';
+$_lang['ms2_tab_comments'] = 'Комментарии';
+$_lang['ms2_tab_product'] = 'Товар';
+$_lang['ms2_tab_product_data'] = 'Свойства товара';
+$_lang['ms2_tab_product_options'] = 'Опции товара';
+$_lang['ms2_tab_product_links'] = 'Связи';
+$_lang['ms2_tab_product_categories'] = 'Категории';
+$_lang['ms2_tab_product_gallery'] = 'Галерея';
