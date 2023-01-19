b564cbc69e058b3a7e3981e072ad6d0cbc411ecc
Ruslan Aleev <aleev.insmile@gmail.com>
2023-01-19 19:56:40 +0400

    New translations product.inc.php (Dutch)

--- core/components/minishop2/lexicon/nl/product.inc.php
+++ core/components/minishop2/lexicon/nl/product.inc.php
@@ -1,168 +1,155 @@
 <?php
 
 /**
- * Products English Lexicon Entries for miniShop2
- *
- * @package minishop2
- * @subpackage lexicon
- */
+* Products Russian Lexicon Entries for miniShop2
+*
+* @package minishop2
+* @subpackage lexicon
+*/
 
-$_lang['ms2_product_tab_main'] = 'Webpagina';
-$_lang['ms2_product_tab_extra'] = 'Product eigenschappen';
-$_lang['ms2_product_tab_seo'] = 'SEO opties';
-$_lang['ms2_product_tab_gallery'] = 'Gallerij';
+$_lang['ms2_product_tab_main'] = 'Web-Dokument';
+$_lang['ms2_product_tab_extra'] = 'Produktspezifikationen';
+$_lang['ms2_product_tab_seo'] = 'SEO Einstellungen';
+$_lang['ms2_product_tab_gallery'] = 'Galerie';
 $_lang['ms2_product_tab_links'] = 'Links';
-$_lang['ms2_product_sections'] = 'Secties';
-$_lang['ms2_product_properties'] = 'Hoofd eigenschappen';
-$_lang['ms2_product_tv'] = 'Optionele eigenschappen';
+$_lang['ms2_product_sections'] = 'Sektionen';
+$_lang['ms2_product_properties'] = 'Hauptspezifikationen';
+$_lang['ms2_product_tv'] = 'Zusätzliche Spezifikationen';
 
 $_lang['ms2_product_id'] = 'ID';
-$_lang['ms2_product_pagetitle'] = 'Naam';
-$_lang['ms2_product_parent'] = 'Categorie';
-$_lang['ms2_product_longtitle'] = 'Volledige naam';
-$_lang['ms2_product_description'] = 'Omschrijving';
+$_lang['ms2_product_pagetitle'] = 'Pagetitle';
+$_lang['ms2_product_parent'] = 'Category';
+$_lang['ms2_product_longtitle'] = 'Longtitle';
+$_lang['ms2_product_description'] = 'Description';
 $_lang['ms2_product_alias'] = 'Alias';
-$_lang['ms2_product_introtext'] = 'Introtekst';
+$_lang['ms2_product_introtext'] = 'Introtext';
 $_lang['ms2_product_content'] = 'Content';
 $_lang['ms2_product_template'] = 'Template';
 $_lang['ms2_product_richtext'] = 'Richtext';
-$_lang['ms2_product_searchable'] = 'Zoekbaar';
-$_lang['ms2_product_cacheable'] = 'Cachebaar';
-$_lang['ms2_product_link_attributes'] = 'Link attributen';
-$_lang['ms2_product_menutitle'] = 'Menu titel';
-$_lang['ms2_product_menuindex'] = 'Menu index';
+$_lang['ms2_product_searchable'] = 'Searchable';
+$_lang['ms2_product_cacheable'] = 'Cacheable';
+$_lang['ms2_product_link_attributes'] = 'Link Attributes';
+$_lang['ms2_product_menutitle'] = 'Menü';
+$_lang['ms2_product_menuindex'] = 'Позиция в меню';
 
-$_lang['ms2_product_published'] = 'Gepubliceerd';
-$_lang['ms2_product_deleted'] = 'Verwijderd';
-$_lang['ms2_product_pub_date'] = 'Publicatiedatum';
-$_lang['ms2_product_unpub_date'] = 'Depublicatiedatum';
+$_lang['ms2_product_deleted'] = 'Gelöscht';
+$_lang['ms2_product_published'] = 'Veröffentlicht';
+$_lang['ms2_product_pub_date'] = 'Publish Date';
+$_lang['ms2_product_unpub_date'] = 'Unpublish date';
 
-$_lang['ms2_product_createdby'] = 'Aangemaakt door';
-$_lang['ms2_product_createdby_help'] = 'Gebruiker welke de pagina heeft aangemaakt.';
-$_lang['ms2_product_createdon'] = 'Aangemaakt op';
-$_lang['ms2_product_createdon_help'] = 'Datum wanneer de pagina is aangemaakt.';
-$_lang['ms2_product_editedby'] = 'Aangepast door';
-$_lang['ms2_product_editedby_help'] = 'Gebruiker welke de pagina als laatste heeft bijgewerkt.';
-$_lang['ms2_product_editedon'] = 'Aangepast op';
-$_lang['ms2_product_editedon_help'] = 'Datum wanneer de laatste wijziging heeft plaatsgevonden.';
-$_lang['ms2_product_deletedon'] = 'Verwijderd op';
-$_lang['ms2_product_deletedon_help'] = 'Datum waarop de pagina is verwijderd.';
-$_lang['ms2_product_deletedby'] = 'Verwijderd door';
-$_lang['ms2_product_deletedby_help'] = 'Gebruiker welke de pagina heeft verwijderd.';
-$_lang['ms2_product_publishedon'] = 'Gepubliceerd op';
-$_lang['ms2_product_publishedon_help'] = 'Datum waarop de pagina is gepubliceerd.';
-$_lang['ms2_product_publishedby'] = 'Gepubliceerd door';
-$_lang['ms2_product_publishedby_help'] = 'Gebruiker welke de pagina heeft gepubliceerd.';
+$_lang['ms2_product_createdby'] = 'Erstellt von';
+$_lang['ms2_product_createdby_help'] = 'Benutzer, der die Resource erstellt hat.';
+$_lang['ms2_product_createdon'] = 'Erstellt am';
+$_lang['ms2_product_createdon_help'] = 'Datum der Erstellung der Resource.';
+$_lang['ms2_product_editedby'] = 'Bearbeitet von';
+$_lang['ms2_product_editedby_help'] = 'Benutzer, der die Resource zuletzt bearbeitet hat.';
+$_lang['ms2_product_editedon'] = 'Bearbeitet am';
+$_lang['ms2_product_editedon_help'] = 'Datum der letzten Bearbeitung der Resource.';
+$_lang['ms2_product_deletedon'] = 'Gelöscht am';
+$_lang['ms2_product_deletedon_help'] = 'Datum der Löschung der Resource.';
+$_lang['ms2_product_deletedby'] = 'Gelöscht von';
+$_lang['ms2_product_deletedby_help'] = 'Benutzer, der die Resource gelöscht hat.';
+$_lang['ms2_product_publishedon'] = 'Veröffentlicht am';
+$_lang['ms2_product_publishedon_help'] = 'Datum der Veröffentlichung der Resource.';
+$_lang['ms2_product_publishedby'] = 'Veröffentlicht von';
+$_lang['ms2_product_publishedby_help'] = 'Benutzer, der die Resource veröffentlicht hat.';
 
-$_lang['ms2_product_menutitle'] = 'Menu title';
-$_lang['ms2_product_hidemenu'] = 'Verberg in menu';
-$_lang['ms2_product_uri_override'] = 'Overschrijvende URI';
+$_lang['ms2_product_menutitle'] = 'Пункт меню';
+$_lang['ms2_product_hidemenu'] = 'Hide in menu';
+$_lang['ms2_product_uri_override'] = 'Override URI';
 $_lang['ms2_product_uri'] = 'URI';
-$_lang['ms2_product_hide_children_in_tree'] = 'Verberg subpagina\'s in de boomstructuur';
-$_lang['ms2_product_hide_children_in_tree_help'] = 'Deze optie zal alle producten en categorieën verbergen in de boomstructuur. Het overschrijft de subpagina optie "show_in_tree".';
-$_lang['ms2_product_hide_in_tree'] = 'Verberg in boomstructuur';
-$_lang['ms2_product_show_in_tree'] = 'Toon in boomstructuur';
-$_lang['ms2_product_show_in_tree_help'] = 'Standaard worden goederen niet getoond in de MODX boomstructuur in verband met snelheid. Het tonen van deze goederen kun je echter op sommige plekken inschakelen.';
-$_lang['ms2_product_syncsite'] = 'Cache legen';
+$_lang['ms2_product_hide_children_in_tree'] = 'Скрыть потомков в дереве';
+$_lang['ms2_product_hide_children_in_tree_help'] = 'Эта настройка скроет все дочерние товары и категории в дереве ресурса, независимо от их свойства "show_in_tree".';
+$_lang['ms2_product_hide_in_tree'] = 'Скрыть в дереве';
+$_lang['ms2_product_show_in_tree'] = 'Show in tree';
+$_lang['ms2_product_show_in_tree_help'] = 'By default, the goods is not displayed in the MODX resource tree for speed reasons. But you can enable displaying of some positions.';
+$_lang['ms2_product_syncsite'] = 'Cache leeren';
 
-$_lang['ms2_product_article'] = 'Artikel';
+$_lang['ms2_product_article'] = 'Artikel-Nr.';
 $_lang['ms2_product_article_help'] = '';
-$_lang['ms2_product_price'] = 'Prijs';
+$_lang['ms2_product_price'] = 'Preis';
 $_lang['ms2_product_price_help'] = '';
-$_lang['ms2_product_old_price'] = 'Oude prijs';
+$_lang['ms2_product_old_price'] = 'Alter Preis';
 $_lang['ms2_product_old_price_help'] = '';
 $_lang['ms2_product_weight'] = 'Gewicht';
 $_lang['ms2_product_weight_help'] = '';
-$_lang['ms2_product_color'] = 'Kleuren';
+$_lang['ms2_product_color'] = 'Farben';
 $_lang['ms2_product_color_help'] = '';
-$_lang['ms2_product_remains'] = 'Voorraad';
+$_lang['ms2_product_remains'] = 'Verbleibend';
 $_lang['ms2_product_remains_help'] = '';
-$_lang['ms2_product_reserved'] = 'Gereserveerd';
+$_lang['ms2_product_reserved'] = 'Reserviert';
 $_lang['ms2_product_reserved_help'] = '';
-$_lang['ms2_product_image'] = 'Afbeelding';
+$_lang['ms2_product_image'] = 'Bild';
 $_lang['ms2_product_image_help'] = '';
-$_lang['ms2_product_thumb'] = 'Thumbnail';
+$_lang['ms2_product_thumb'] = 'Vorschaubild';
 $_lang['ms2_product_thumb_help'] = '';
-$_lang['ms2_product_vendor'] = 'Leverancier';
+$_lang['ms2_product_vendor'] = 'Hersteller';
 $_lang['ms2_product_vendor_help'] = '';
-$_lang['ms2_product_made_in'] = 'Gemaakt in';
+$_lang['ms2_product_made_in'] = 'Hergestellt in';
 $_lang['ms2_product_made_in_help'] = '';
 $_lang['ms2_product_tags'] = 'Tags';
-$_lang['ms2_product_tags_help'] = 'Tags gekoppeld aan het product.';
-$_lang['ms2_product_size'] = 'Afmetingen';
-$_lang['ms2_product_size_help'] = 'Afmetingen van het product.';
-$_lang['ms2_product_color'] = 'Colors';
-$_lang['ms2_product_color_help'] = 'Any colors of the product.';
-$_lang['ms2_product_new'] = 'Nieuw';
+$_lang['ms2_product_tags_help'] = 'Produkt-Tags.';
+$_lang['ms2_product_size'] = 'Größen';
+$_lang['ms2_product_size_help'] = 'Größen des Produkts.';
+$_lang['ms2_product_color'] = 'Цвета';
+$_lang['ms2_product_color_help'] = 'Цвета товара, если есть.';
+$_lang['ms2_product_new'] = 'Neu';
 $_lang['ms2_product_new_help'] = '';
-$_lang['ms2_product_favorite'] = 'Favoriet';
+$_lang['ms2_product_favorite'] = 'Favorit';
 $_lang['ms2_product_favorite_help'] = '';
-$_lang['ms2_product_popular'] = 'Populair';
+$_lang['ms2_product_popular'] = 'Beliebt';
 $_lang['ms2_product_popular_help'] = '';
-$_lang['ms2_product_source'] = 'Media source';
-$_lang['ms2_product_source_help'] = 'De media source voor het opslaan van productafbeeldingen.';
-$_lang['ms2_product_change_source_confirm'] = 'Weet je zeker dat je de media source van het product wilt wijzigen? WAARSCHUWING: Dit zal voorafgaande wijzingen opslaan, wees er zeker van dat u dit wilt voordat u verder gaat.';
-$_lang['ms2_product_count'] = 'Aantal';
-$_lang['ms2_product_options'] = 'Product opties';
+$_lang['ms2_product_source'] = 'Medienquelle';
+$_lang['ms2_product_source_help'] = 'Medienquelle, die die Produktbilder speichert.';
+$_lang['ms2_product_change_source_confirm'] = 'Wollen Sie die Medienquelle dieses Produktes wirklich ändern? ACHTUNG: Diese Aktion wird Ihre vorherigen Änderungen speichern!';
+$_lang['ms2_product_count'] = 'Количество';
+$_lang['ms2_product_options'] = 'Опции товаров';
 
-$_lang['ms2_product_save'] = 'Opslaan';
-$_lang['ms2_product_edit'] = 'Wijzigen';
-$_lang['ms2_product_view'] = 'Bekijken';
-$_lang['ms2_product_delete'] = 'Verwijderen';
-$_lang['ms2_product_undelete'] = 'Verwijderen ongedaan maken';
-$_lang['ms2_product_publish'] = 'Publiceren';
-$_lang['ms2_product_unpublish'] = 'Depubliceren';
-$_lang['ms2_product_duplicate'] = 'Dupliceren';
-$_lang['ms2_product_cancel'] = 'Annuleren';
-$_lang['ms2_product_back'] = 'Terug';
-$_lang['ms2_product_prev'] = 'Vorige product';
-$_lang['ms2_product_next'] = 'Volgende product';
-$_lang['ms2_product_help'] = 'Hulp';
+$_lang['ms2_product_save'] = 'Speichern';
+$_lang['ms2_product_edit'] = 'Bearbeiten';
+$_lang['ms2_product_view'] = 'Ansehen';
+$_lang['ms2_product_delete'] = 'Löschen';
+$_lang['ms2_product_undelete'] = 'Wiederherstellen';
+$_lang['ms2_product_publish'] = 'Veröffentlichen';
+$_lang['ms2_product_unpublish'] = 'Veröffentlichung zurückziehen';
+$_lang['ms2_product_duplicate'] = 'Копировать';
+$_lang['ms2_product_cancel'] = 'Abbrechen';
+$_lang['ms2_product_back'] = 'Zurück';
+$_lang['ms2_product_prev'] = 'Vorheriges Produkt';
+$_lang['ms2_product_next'] = 'Nächstes Produkt';
+$_lang['ms2_product_help'] = 'Hilfe';
 
-$_lang['ms2_product_duplicate_confirm'] = 'Weet je zeker dat je een kopie wilt maken van dit product?';
-$_lang['ms2_product_delete_desc'] = 'Weet je zeker dat je dit product wilt verwijderen?';
-$_lang['ms2_product_selected_publish'] = 'Publiceer geselecteerde producten';
-$_lang['ms2_product_selected_unpublish'] = 'Depubliceer geselecteerde producten';
-$_lang['ms2_product_selected_delete'] = 'Verwijder geselecteerde producten';
-$_lang['ms2_product_selected_undelete'] = 'Verwijderen geselecteerde producten ongedaan maken';
+$_lang['ms2_product_duplicate_confirm'] = 'Produkt wirklich duplizieren?';
+$_lang['ms2_product_delete_desc'] = 'Dieses Produkt wirklich löschen?';
+$_lang['ms2_product_selected_publish'] = 'Ausgewählte Produkte veröffentlichen';
+$_lang['ms2_product_selected_unpublish'] = 'Ausgewählte Produkte zurückziehen';
+$_lang['ms2_product_selected_delete'] = 'Ausgewählte Produkte löschen';
+$_lang['ms2_product_selected_undelete'] = 'Ausgewählte Produkte wiederherstellen';
 
-//$_lang['ms2_disabled_while_creating'] = '<i>This function is disabled when creating a new product</i>';
+//$_lang['ms2_disabled_while_creating'] = '<i>Эта функция отключена при создании нового товара.</i>';
 
-$_lang['ms2_gallery_emptymsg'] = '<p style="padding: 20px 0 0 5px;color:#555;">Bestanden niet gevonden.<br/><br/>Je kunt deze uploaden door ze hier naartoe te slepen of via de knop bovenin.</p>';
-$_lang['ms2_gallery_file_name'] = 'Bestandsnaam';
+$_lang['ms2_gallery_emptymsg'] = 'Für Bilderupload gewünschte Bilder per Drag & Drop auf dieses Feld ziehen oder Button benutzen.';
 $_lang['ms2_gallery_unavailablemsg'] = 'To upload files to the Gallery, you must first create (save) the product';
-$_lang['ms2_gallery_file_title'] = 'Titel bestand';
-$_lang['ms2_gallery_file_description'] = 'Omschrijving';
-$_lang['ms2_gallery_file_size'] = 'Bestandsgrootte';
-$_lang['ms2_gallery_file_createdon'] = 'Aangemaakt op';
-$_lang['ms2_gallery_file_createdby'] = 'Aangemaakt door';
-$_lang['ms2_gallery_file_url'] = 'Url';
-$_lang['ms2_gallery_file_source'] = 'Media source';
+$_lang['ms2_gallery_file_name'] = 'Имя файла';
+$_lang['ms2_gallery_file_title'] = 'Название';
+$_lang['ms2_gallery_file_description'] = 'Описание';
+$_lang['ms2_gallery_file_size'] = 'Размер';
+$_lang['ms2_gallery_file_createdon'] = 'Дата создания';
+$_lang['ms2_gallery_file_createdby'] = 'Автор';
+$_lang['ms2_gallery_file_url'] = 'Ссылка';
+$_lang['ms2_gallery_file_source'] = 'Источник файла';
 
-$_lang['ms2_gallery_button_upload'] = 'Selecteer bestanden';
+$_lang['ms2_gallery_button_upload'] = 'Dateien wählen';
 
-$_lang['ms2_gallery_file_show'] = 'Open in een nieuw venster';
-$_lang['ms2_gallery_file_update'] = 'Eigenschappen bijwerken';
-$_lang['ms2_gallery_file_generate_all'] = 'Update all';
-$_lang['ms2_gallery_file_generate_thumbs'] = 'Thumbnails opnieuw genereren';
-$_lang['ms2_gallery_file_generate_thumbs_confirm'] = 'Are you shure you want to regenerate thumbnails of all files?';
-$_lang['ms2_gallery_file_delete'] = 'Verwijder bestand';
-$_lang['ms2_gallery_file_delete_all'] = 'Delete all';
-$_lang['ms2_gallery_file_delete_confirm'] = 'Weet je zeker dat je dit bestand wil verwijderen inclusief alle verwante thumbnails?<br/> Deze bewerking is onomkeerbaar.';
-$_lang['ms2_gallery_file_delete_multiple'] = 'Verwijder bestanden';
-$_lang['ms2_gallery_file_delete_multiple_confirm'] = 'Weet je zeker dat je deze bestanden wil verwijderen inclusief alle verwante thumbnails?<br/> Deze bewerking is onomkeerbaar.';
+$_lang['ms2_gallery_file_show'] = 'Открыть в новом окне';
+$_lang['ms2_gallery_file_update'] = 'Name & Beschreibung bearbeiten';
+$_lang['ms2_gallery_file_generate_all'] = 'Обновить все';
+$_lang['ms2_gallery_file_generate_thumbs'] = 'Обновить превьюшки';
+$_lang['ms2_gallery_file_generate_thumbs_confirm'] = 'Вы действительно хотите обновить превью всех файлов?';
+$_lang['ms2_gallery_file_delete'] = 'Datei löschen';
+$_lang['ms2_gallery_file_delete_all'] = 'Удалить все';
+$_lang['ms2_gallery_file_delete_confirm'] = 'Dieses Datei inklusive Thumbnail löschen? <br/> Diese Aktion kann nicht rückgängig gemacht werden.';
+$_lang['ms2_gallery_file_delete_multiple'] = 'Dateien löschen';
+$_lang['ms2_gallery_file_delete_multiple_confirm'] = 'Diese Datei inklusive Thumbnails löschen? <br/> Diese Aktion kann nicht rückgängig gemacht werden.';
 
-$_lang['ms2_gallery_errors'] = 'Fouten tijdens het uploaden';
-
-$_lang['ms2_drag_self_one'] = 'Cancel';
-$_lang['ms2_drag_self_many'] = 'Cancel';
-$_lang['ms2_drag_move_one'] = 'Move product to category <strong>«{0}»</strong>';
-$_lang['ms2_drag_move_one_success'] = 'Product was successfully moved to category <strong>«{0}»</strong>';
-$_lang['ms2_drag_move_many'] = 'Move products to category <strong>«{0}»</strong>';
-$_lang['ms2_drag_move_many_success'] = 'Products was successfully moved to category <strong>«{0}»</strong>';
-$_lang['ms2_drag_move_current_one'] = 'Move product to current category';
-$_lang['ms2_drag_move_current_once_success'] = 'Product was successfully moved to current category';
-$_lang['ms2_drag_move_current_many'] = 'Move products to current category';
-$_lang['ms2_drag_move_current_many_success'] = 'Products was successfully moved to current category';
-$_lang['ms2_drag_sort_one'] = 'Sort product';
-$_lang['ms2_drag_sort_many'] = 'Sorts product';
+$_lang['ms2_gallery_errors'] = 'Ошибки при загрузке';
