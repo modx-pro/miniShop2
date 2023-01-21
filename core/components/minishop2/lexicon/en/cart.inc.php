1b8fe29addf985062e9055e31ecc5fac038d6076
Ruslan Aleev <aleev.insmile@gmail.com>
2023-01-21 19:27:58 +0400

    New translations cart.inc.php (English)

--- core/components/minishop2/lexicon/en/cart.inc.php
+++ core/components/minishop2/lexicon/en/cart.inc.php
@@ -1,43 +1,37 @@
 <?php
 
 /**
- * Default English Lexicon Entries for miniShop2 cart
+ * Cart Lexicon Entries
+ * Sorted by key, alphabetically
  *
  * @package minishop2
  * @subpackage lexicon
  */
 
 $_lang['ms2_cart'] = 'Cart';
-$_lang['ms2_minicart'] = 'Cart';
-
-$_lang['ms2_minicart_goods'] = 'Total items:';
-$_lang['ms2_minicart_cost'] = 'Total cost:';
-$_lang['ms2_minicart_is_empty'] = 'Your cart is empty';
-
-$_lang['ms2_cart_title'] = 'Title';
-$_lang['ms2_cart_price'] = 'Price';
-$_lang['ms2_cart_weight'] = 'Weight';
-$_lang['ms2_cart_count'] = 'Quantity';
-$_lang['ms2_cart_cost'] = 'Cost';
-$_lang['ms2_cart_remove'] = 'Remove';
-$_lang['ms2_cart_total'] = 'Total';
-
-$_lang['ms2_cart_add_success'] = 'Product has been added to cart: [[+count]]';
-$_lang['ms2_cart_add_error'] = 'Error when adding to the cart';
+$_lang['ms2_cart_add_err_count'] = 'Error when adding to the cart: wrong count "[[+count]]"';
 $_lang['ms2_cart_add_err_id'] = 'Error when adding to the cart: id of product is not set';
 $_lang['ms2_cart_add_err_nf'] = 'Error when adding to the cart: product not found';
-$_lang['ms2_cart_add_err_count'] = 'Error when adding to the cart: wrong count "[[+count]]"';
-
-$_lang['ms2_cart_remove_success'] = 'Product was removed from cart';
-$_lang['ms2_cart_remove_error'] = 'Error when removing product from cart';
-
-$_lang['ms2_cart_change_success'] = 'Quantity of the product in cart was successfully changed: [[+count]]';
+$_lang['ms2_cart_add_error'] = 'Error when adding to the cart';
+$_lang['ms2_cart_add_success'] = 'Product has been added to cart: [[+count]]';
 $_lang['ms2_cart_change_error'] = 'Error when changing product count in cart';
-
-$_lang['ms2_cart_clean_success'] = 'Cart was successfully cleared';
-$_lang['ms2_cart_is_empty'] = 'Your cart is empty';
+$_lang['ms2_cart_change_success'] = 'Quantity of the product in cart was successfully changed: [[+count]]';
 $_lang['ms2_cart_clean'] = 'Clear shopping cart';
-
-$_lang['ms2_order_err_empty'] = 'Can\'t submit an empty order';
+$_lang['ms2_cart_clean_success'] = 'Cart was successfully cleared';
+$_lang['ms2_cart_cost'] = 'Cost';
+$_lang['ms2_cart_count'] = 'Quantity';
+$_lang['ms2_cart_is_empty'] = 'Your cart is empty';
+$_lang['ms2_cart_price'] = 'Price';
+$_lang['ms2_cart_remove'] = 'Remove';
+$_lang['ms2_cart_remove_error'] = 'Error when removing product from cart';
+$_lang['ms2_cart_remove_success'] = 'Product was removed from cart';
+$_lang['ms2_cart_title'] = 'Title';
+$_lang['ms2_cart_total'] = 'Total';
+$_lang['ms2_cart_weight'] = 'Weight';
+$_lang['ms2_minicart'] = 'Cart';
+$_lang['ms2_minicart_cost'] = 'Total cost:';
+$_lang['ms2_minicart_goods'] = 'Total items:';
+$_lang['ms2_minicart_is_empty'] = 'Your cart is empty';
 $_lang['ms2_order_err_delivery'] = 'You must select a shipping method';
+$_lang['ms2_order_err_empty'] = 'Can\'t submit an empty order';
 $_lang['ms2_order_err_requires'] = 'You must fill in the highlighted fields';
