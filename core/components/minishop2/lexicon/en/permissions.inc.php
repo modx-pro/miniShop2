8fa5c1096a2f03fb53284af2f5ff68d8fe29e272
Ruslan Aleev <aleev.insmile@gmail.com>
2023-01-21 19:28:24 +0400

    New translations permissions.inc.php (English)

--- core/components/minishop2/lexicon/en/permissions.inc.php
+++ core/components/minishop2/lexicon/en/permissions.inc.php
@@ -1,27 +1,24 @@
 <?php
 
 /**
- * Permissions English Lexicon Entries to miniShop2
+ * Permissions Lexicon Entries
+ * Sorted by key, alphabetically
  *
  * @package minishop2
  * @subpackage lexicon
  */
 
 $_lang['mscategory_save'] = 'Permission to save\update minishop category';
-$_lang['msproduct_save'] = 'Permission to save\update minishop product';
-
-$_lang['msorder_view'] = 'Permission to view order';
 $_lang['msorder_list'] = 'Permission to list of orders';
-$_lang['msorder_save'] = 'Permission to update order';
 $_lang['msorder_remove'] = 'Permission to remove order';
-
-$_lang['mssetting_view'] = 'Permission to view of settings';
-$_lang['mssetting_list'] = 'Permission to list of settings';
-$_lang['mssetting_save'] = 'Permission to save\update settings';
-
-$_lang['msproductfile_save'] = 'Permission to save\update product files';
-$_lang['msproductfile_generate'] = 'Permission to generate thumbnails of product images';
-$_lang['msproductfile_list'] = 'Permission to list product files';
-
+$_lang['msorder_save'] = 'Permission to update order';
+$_lang['msorder_view'] = 'Permission to view order';
 $_lang['msproduct_delete'] = 'Permission to remove product';
 $_lang['msproduct_publish'] = 'Permission to publish product';
+$_lang['msproduct_save'] = 'Permission to save\update minishop product';
+$_lang['msproductfile_generate'] = 'Permission to generate thumbnails of product images';
+$_lang['msproductfile_list'] = 'Permission to list product files';
+$_lang['msproductfile_save'] = 'Permission to save\update product files';
+$_lang['mssetting_list'] = 'Permission to list of settings';
+$_lang['mssetting_save'] = 'Permission to save\update settings';
+$_lang['mssetting_view'] = 'Permission to view of settings';
