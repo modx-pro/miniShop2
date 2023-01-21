dcf69ef60655f5b0e6cf12ecfb9ba84dc548c958
Ruslan Aleev <aleev.insmile@gmail.com>
2023-01-21 19:27:52 +0400

    New translations cart.inc.php (French)

--- core/components/minishop2/lexicon/fr/cart.inc.php
+++ core/components/minishop2/lexicon/fr/cart.inc.php
@@ -1,42 +1,37 @@
 <?php
 
 /**
- * Default French Lexicon Entries for miniShop2 cart
+ * Cart Lexicon Entries
+ * Sorted by key, alphabetically
  *
  * @package minishop2
  * @subpackage lexicon
  */
 
 $_lang['ms2_cart'] = 'Panier';
-$_lang['ms2_minicart'] = 'Panier';
-
-$_lang['ms2_minicart_goods'] = 'Tous les articles :';
-$_lang['ms2_minicart_cost'] = 'pour un montant de :';
-$_lang['ms2_minicart_is_empty'] = 'Votre panier est vide';
-
-$_lang['ms2_cart_title'] = 'Titre';
-$_lang['ms2_cart_price'] = 'Prix';
-$_lang['ms2_cart_weight'] = 'Poids';
-$_lang['ms2_cart_count'] = 'Nombre';
-$_lang['ms2_cart_cost'] = 'Montant';
-$_lang['ms2_cart_remove'] = 'Enlever';
-$_lang['ms2_cart_total'] = 'Total';
-
-$_lang['ms2_cart_add_success'] = 'L\'article a été ajouté dans le panier: [[+count]]';
-$_lang['ms2_cart_add_error'] = 'Erreur lors de l\'ajout dans le panier';
+$_lang['ms2_cart_add_err_count'] = 'Erreur lors de l\'ajout dans le panier: trop d\'articles - [[+count]]';
 $_lang['ms2_cart_add_err_id'] = 'Erreur lors de l\'ajout dans le panier: id de l\'article n\'est pas définit';
 $_lang['ms2_cart_add_err_nf'] = 'Erreur lors de l\'ajout dans le panier: article non trouvé';
-$_lang['ms2_cart_add_err_count'] = 'Erreur lors de l\'ajout dans le panier: trop d\'articles - [[+count]]';
-
-$_lang['ms2_cart_remove_success'] = 'L\'article a été supprimé du panier';
-$_lang['ms2_cart_remove_error'] = 'Erreur lors de la suppression de l\'article du panier';
-
-$_lang['ms2_cart_change_success'] = 'La quantité de l\'article a été modifiée avec succés : [[+count]]';
+$_lang['ms2_cart_add_error'] = 'Erreur lors de l\'ajout dans le panier';
+$_lang['ms2_cart_add_success'] = 'L\'article a été ajouté dans le panier: [[+count]]';
 $_lang['ms2_cart_change_error'] = 'Erreur lors de la modification du nombre d\'article dans le panier';
-
-$_lang['ms2_cart_clean_success'] = 'Le panier a été vidé avec succés';
-$_lang['ms2_cart_is_empty'] = 'Votre panier est vide';
+$_lang['ms2_cart_change_success'] = 'La quantité de l\'article a été modifiée avec succés : [[+count]]';
 $_lang['ms2_cart_clean'] = 'Nettoyage du panier';
-
+$_lang['ms2_cart_clean_success'] = 'Le panier a été vidé avec succés';
+$_lang['ms2_cart_cost'] = 'Montant';
+$_lang['ms2_cart_count'] = 'Nombre';
+$_lang['ms2_cart_is_empty'] = 'Votre panier est vide';
+$_lang['ms2_cart_price'] = 'Prix';
+$_lang['ms2_cart_remove'] = 'Enlever';
+$_lang['ms2_cart_remove_error'] = 'Erreur lors de la suppression de l\'article du panier';
+$_lang['ms2_cart_remove_success'] = 'L\'article a été supprimé du panier';
+$_lang['ms2_cart_title'] = 'Titre';
+$_lang['ms2_cart_total'] = 'Total';
+$_lang['ms2_cart_weight'] = 'Poids';
+$_lang['ms2_minicart'] = 'Panier';
+$_lang['ms2_minicart_cost'] = 'pour un montant de :';
+$_lang['ms2_minicart_goods'] = 'Tous les articles :';
+$_lang['ms2_minicart_is_empty'] = 'Votre panier est vide';
 $_lang['ms2_order_err_delivery'] = 'Vous devez choisir une méthode de livraison';
+$_lang['ms2_order_err_empty'] = 'Невозможно оформить пустой заказ';
 $_lang['ms2_order_err_requires'] = 'Vous devez remplir les champs neccessaires';
