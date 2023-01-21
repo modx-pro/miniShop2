b01eab6c151278a5b39c91629dccba4df2a170fa
Ruslan Aleev <aleev.insmile@gmail.com>
2023-01-21 19:28:20 +0400

    New translations permissions.inc.php (Italian)

--- core/components/minishop2/lexicon/it/permissions.inc.php
+++ core/components/minishop2/lexicon/it/permissions.inc.php
@@ -1,23 +1,24 @@
 <?php
 
 /**
- * Permissions English Lexicon Entries to miniShop2
+ * Permissions Lexicon Entries
+ * Sorted by key, alphabetically
  *
  * @package minishop2
  * @subpackage lexicon
  */
 
 $_lang['mscategory_save'] = 'Permesso di salvare/modificare categoria minishop';
-$_lang['msproduct_save'] = 'Permesso di salvare/modificare prodotto minishop';
-
-$_lang['msorder_view'] = 'Permesso di visualizzare l\'ordine';
 $_lang['msorder_list'] = 'Permesso di visualizzare la lista degli ordini';
+$_lang['msorder_remove'] = 'Разрешает удаление заказа';
 $_lang['msorder_save'] = 'Permesso di aggiornare ordine';
-
-$_lang['mssetting_view'] = 'Teisė peržiūrėti nuostatas';
-$_lang['mssetting_list'] = 'Teisė peržiūrėti nuostatų sąrašą';
-$_lang['mssetting_save'] = 'Teisė kurti/redaguoti nuostatas';
-
-$_lang['msproductfile_save'] = 'Teisė įrašyti/atnaujinti prekių failus';
+$_lang['msorder_view'] = 'Permesso di visualizzare l\'ordine';
+$_lang['msproduct_delete'] = 'Разрешает удаление товара';
+$_lang['msproduct_publish'] = 'Разрешает публикацию товара';
+$_lang['msproduct_save'] = 'Permesso di salvare/modificare prodotto minishop';
 $_lang['msproductfile_generate'] = 'Teisė generuoti prekių nuotraukų miniatiūras';
 $_lang['msproductfile_list'] = 'Teisė peržiūrėti prekių failų sąrašą';
+$_lang['msproductfile_save'] = 'Teisė įrašyti/atnaujinti prekių failus';
+$_lang['mssetting_list'] = 'Teisė peržiūrėti nuostatų sąrašą';
+$_lang['mssetting_save'] = 'Teisė kurti/redaguoti nuostatas';
+$_lang['mssetting_view'] = 'Teisė peržiūrėti nuostatas';
