ca7099a778ea9daa7e2a3d028119a55b17015ddb
Ruslan Aleev <aleev.insmile@gmail.com>
2023-01-21 19:28:23 +0400

    New translations permissions.inc.php (Ukrainian)

--- core/components/minishop2/lexicon/uk/permissions.inc.php
+++ core/components/minishop2/lexicon/uk/permissions.inc.php
@@ -1,27 +1,24 @@
 <?php
 
 /**
- * Permissions Ukrainian Lexicon Entries for miniShop2
+ * Permissions Lexicon Entries
+ * Sorted by key, alphabetically
  *
  * @package minishop2
  * @subpackage lexicon
  */
 
 $_lang['mscategory_save'] = 'Дозволяє створення\зміну категорії магазина';
-$_lang['msproduct_save'] = 'Дозволяє створення\зміну товару магазина';
-
-$_lang['msorder_view'] = 'Дозволяє перегляд замовлення';
 $_lang['msorder_list'] = 'Дозволяє показ списку замовлень';
-$_lang['msorder_save'] = 'Дозволяє зміну замовлення';
 $_lang['msorder_remove'] = 'Дозволяє видалення замовлення';
-
-$_lang['mssetting_view'] = 'Дозволяє перегляд налаштувань';
-$_lang['mssetting_list'] = 'Дозволяє показ списку налаштувань';
-$_lang['mssetting_save'] = 'Дозволяє створення\зміну налаштувань';
-
-$_lang['msproductfile_save'] = 'Дозволяє створення\зміну файлів товару';
-$_lang['msproductfile_generate'] = 'Дозволяє геренрацію мініатюр зображень товару';
-$_lang['msproductfile_list'] = 'Дозволяє показ списку файлів товару';
-
+$_lang['msorder_save'] = 'Дозволяє зміну замовлення';
+$_lang['msorder_view'] = 'Дозволяє перегляд замовлення';
 $_lang['msproduct_delete'] = 'Дозволяє видалення товару';
 $_lang['msproduct_publish'] = 'Дозволяє публікацію товару';
+$_lang['msproduct_save'] = 'Дозволяє створення\зміну товару магазина';
+$_lang['msproductfile_generate'] = 'Дозволяє геренрацію мініатюр зображень товару';
+$_lang['msproductfile_list'] = 'Дозволяє показ списку файлів товару';
+$_lang['msproductfile_save'] = 'Дозволяє створення\зміну файлів товару';
+$_lang['mssetting_list'] = 'Дозволяє показ списку налаштувань';
+$_lang['mssetting_save'] = 'Дозволяє створення\зміну налаштувань';
+$_lang['mssetting_view'] = 'Дозволяє перегляд налаштувань';
