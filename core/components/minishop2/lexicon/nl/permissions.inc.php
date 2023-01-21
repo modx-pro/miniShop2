5c850a77dba230704ffbbbcaaaee233d09ef6a2c
Ruslan Aleev <aleev.insmile@gmail.com>
2023-01-21 19:28:22 +0400

    New translations permissions.inc.php (Dutch)

--- core/components/minishop2/lexicon/nl/permissions.inc.php
+++ core/components/minishop2/lexicon/nl/permissions.inc.php
@@ -1,20 +1,24 @@
 <?php
 
 /**
- * Permissions English Lexicon Entries to miniShop2
+ * Permissions Lexicon Entries
+ * Sorted by key, alphabetically
  *
  * @package minishop2
  * @subpackage lexicon
  */
 
 $_lang['mscategory_save'] = 'Toestemming voor het opslaan\bijwerken van een minishop category';
-$_lang['msproduct_save'] = 'Toestemming voor het opslaan\bijwerken van een minishop product';
-$_lang['msorder_view'] = 'Toestemming voor het bekijken van een order';
 $_lang['msorder_list'] = 'Toestemming voor het bekijken van een overzicht van orders';
+$_lang['msorder_remove'] = 'Разрешает удаление заказа';
 $_lang['msorder_save'] = 'Toestemming voor het bijwerken van een order';
-$_lang['mssetting_view'] = 'Toestemming voor het bekijken van de instellingen';
-$_lang['mssetting_list'] = 'Toestemming voor het bekijken van een overzicht van instellingen';
-$_lang['mssetting_save'] = 'Toestemming voor het opslaan\bijwerken van instellingen';
-$_lang['msproductfile_save'] = 'Toestemming voor het opslaan\bijwerken van product bestanden';
+$_lang['msorder_view'] = 'Toestemming voor het bekijken van een order';
+$_lang['msproduct_delete'] = 'Разрешает удаление товара';
+$_lang['msproduct_publish'] = 'Разрешает публикацию товара';
+$_lang['msproduct_save'] = 'Toestemming voor het opslaan\bijwerken van een minishop product';
 $_lang['msproductfile_generate'] = 'Toestemming voor het genereren van thumbnails van product afbeeldingen';
 $_lang['msproductfile_list'] = 'Toestemming voor het tonen van een overzicht van een overzicht van productbestanden';
+$_lang['msproductfile_save'] = 'Toestemming voor het opslaan\bijwerken van product bestanden';
+$_lang['mssetting_list'] = 'Toestemming voor het bekijken van een overzicht van instellingen';
+$_lang['mssetting_save'] = 'Toestemming voor het opslaan\bijwerken van instellingen';
+$_lang['mssetting_view'] = 'Toestemming voor het bekijken van de instellingen';
