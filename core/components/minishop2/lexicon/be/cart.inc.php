a85af27e01cca5b7a7e0840b78510500a2627a07
Ruslan Aleev <aleev.insmile@gmail.com>
2023-01-21 19:27:53 +0400

    New translations cart.inc.php (Belarusian)

--- core/components/minishop2/lexicon/be/cart.inc.php
+++ core/components/minishop2/lexicon/be/cart.inc.php
@@ -1,42 +1,37 @@
 <?php
 
 /**
- * Default Belorussian Lexicon Entries for miniShop2 cart
+ * Cart Lexicon Entries
+ * Sorted by key, alphabetically
  *
  * @package minishop2
  * @subpackage lexicon
  */
 
 $_lang['ms2_cart'] = 'Кошык';
-$_lang['ms2_minicart'] = 'Кошык';
-
-$_lang['ms2_minicart_goods'] = 'Усяго тавараў:';
-$_lang['ms2_minicart_cost'] = 'На суму:';
-$_lang['ms2_minicart_is_empty'] = 'Вы яшчэ нічога не выбралі';
-
-$_lang['ms2_cart_title'] = 'Найменне';
-$_lang['ms2_cart_price'] = 'Цана';
-$_lang['ms2_cart_weight'] = 'Вага';
-$_lang['ms2_cart_count'] = 'Колькасць';
-$_lang['ms2_cart_cost'] = 'Кошт';
-$_lang['ms2_cart_remove'] = 'Выдаліць';
-$_lang['ms2_cart_total'] = 'Усяго';
-
-$_lang['ms2_cart_add_success'] = 'Тавар паспяхова дададзены ў кошык: [[+count]]';
-$_lang['ms2_cart_add_error'] = 'Памылка дадання тавара ў кошык';
+$_lang['ms2_cart_add_err_count'] = 'Памылка дадання ў кошык: няправільная колькасць "[[+count]]"';
 $_lang['ms2_cart_add_err_id'] = 'Памылка дадання ў кошык: не пазначаны ідэнтыфікатар тавару';
 $_lang['ms2_cart_add_err_nf'] = 'Памылка дадання ў кошык: тавар не знойдзены';
-$_lang['ms2_cart_add_err_count'] = 'Памылка дадання ў кошык: няправільная колькасць "[[+count]]"';
-
-$_lang['ms2_cart_remove_success'] = 'Тавар паспяхова выдалены з кошыку';
-$_lang['ms2_cart_remove_error'] = 'Памылка выдалення тавару';
-
-$_lang['ms2_cart_change_success'] = 'Колькасць тавару ў кошыку паспяхова зменена: [[+count]]';
+$_lang['ms2_cart_add_error'] = 'Памылка дадання тавара ў кошык';
+$_lang['ms2_cart_add_success'] = 'Тавар паспяхова дададзены ў кошык: [[+count]]';
 $_lang['ms2_cart_change_error'] = 'Памылка пры змене колькасці тавару ў кошыку';
-
-$_lang['ms2_cart_clean_success'] = 'Кошык паспяхова ачышчаны';
-$_lang['ms2_cart_is_empty'] = 'Ваш кошык пусты';
+$_lang['ms2_cart_change_success'] = 'Колькасць тавару ў кошыку паспяхова зменена: [[+count]]';
 $_lang['ms2_cart_clean'] = 'Ачысціць кошык';
-
+$_lang['ms2_cart_clean_success'] = 'Кошык паспяхова ачышчаны';
+$_lang['ms2_cart_cost'] = 'Кошт';
+$_lang['ms2_cart_count'] = 'Колькасць';
+$_lang['ms2_cart_is_empty'] = 'Ваш кошык пусты';
+$_lang['ms2_cart_price'] = 'Цана';
+$_lang['ms2_cart_remove'] = 'Выдаліць';
+$_lang['ms2_cart_remove_error'] = 'Памылка выдалення тавару';
+$_lang['ms2_cart_remove_success'] = 'Тавар паспяхова выдалены з кошыку';
+$_lang['ms2_cart_title'] = 'Найменне';
+$_lang['ms2_cart_total'] = 'Усяго';
+$_lang['ms2_cart_weight'] = 'Вага';
+$_lang['ms2_minicart'] = 'Кошык';
+$_lang['ms2_minicart_cost'] = 'На суму:';
+$_lang['ms2_minicart_goods'] = 'Усяго тавараў:';
+$_lang['ms2_minicart_is_empty'] = 'Вы яшчэ нічога не выбралі';
 $_lang['ms2_order_err_delivery'] = 'Вы павінны выбраць спосаб дастаўкі';
+$_lang['ms2_order_err_empty'] = 'Немагчыма аформіць пустую замову';
 $_lang['ms2_order_err_requires'] = 'Вы павінны запоўніць неабходныя палі';
