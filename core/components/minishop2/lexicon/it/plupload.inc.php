876d48f7716f6dea1099c88df95634694ba9f8ee
Ruslan Aleev <aleev.insmile@gmail.com>
2023-06-01 10:51:39 +0400

    New translations plupload.inc.php (Italian)

---
+++ core/components/minishop2/lexicon/it/plupload.inc.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * plupload file upload plugin Lexicon Entries
+ * Sorted by key, alphabetically
+ *
+ * @package minishop2
+ * @subpackage lexicon
+ */
+
+$_lang['ms2_plupload_b'] = 'б';
+$_lang['ms2_plupload_close'] = 'Закрыть';
+$_lang['ms2_plupload_drag'] = 'Перетащите файлы сюда.';
+$_lang['ms2_plupload_elements_error'] = 'Вы должны указать browse_button или drop_element.';
+$_lang['ms2_plupload_file'] = 'Файл: %s';
+$_lang['ms2_plupload_file_exist_error'] = '%s уже присутствует в очереди.';
+$_lang['ms2_plupload_file_exist_queue_error'] = 'Такой файл уже присутствует в очереди.';
+$_lang['ms2_plupload_file_extension_error'] = 'Неправильное расширение файла.';
+$_lang['ms2_plupload_file_extension_invalid_error'] = 'Ошибка: У файла неправильное расширение:';
+$_lang['ms2_plupload_file_large_error'] = 'Ошибка: Файл слишком большой:';
+$_lang['ms2_plupload_file_params'] = 'Файл:  %s, размер: %d, макс. размер файла: %d';
+$_lang['ms2_plupload_file_resoultion_error'] = 'Слишком большое разрешение! <b>%s</b> поддерживаются изображения с размером не более %wx%hpx';
+$_lang['ms2_plupload_file_size_error'] = 'Неправильный размер файла.';
+$_lang['ms2_plupload_filename'] = 'Имя файла';
+$_lang['ms2_plupload_files_add'] = 'Добавьте файлы';
+$_lang['ms2_plupload_files_max_error'] = 'Слишком много файлов.';
+$_lang['ms2_plupload_files_queue_add'] = 'Добавьте файлы в очередь и нажмите кнопку "Загрузить файлы".';
+$_lang['ms2_plupload_files_select'] = 'Выберите файлы';
+$_lang['ms2_plupload_format_error'] = 'Формат картинки неправильный или он не поддерживается.';
+$_lang['ms2_plupload_gb'] = 'гб';
+$_lang['ms2_plupload_http_error'] = 'Ошибка HTTP.';
+$_lang['ms2_plupload_init_error'] = 'Ошибка инициализации.';
+$_lang['ms2_plupload_kb'] = 'кб';
+$_lang['ms2_plupload_list'] = 'Список';
+$_lang['ms2_plupload_mb'] = 'мб';
+$_lang['ms2_plupload_memory_limit_error'] = 'Рабочая среда превысила лимит доступной памяти.';
+$_lang['ms2_plupload_na'] = 'N/A';
+$_lang['ms2_plupload_not_found_error'] = '%s существует, но не может быть найден.';
+$_lang['ms2_plupload_queue'] = 'В очереди %d файл(ов)';
+$_lang['ms2_plupload_queue_error'] = 'Загрузочный элемент за раз принимает только %d файл(ов). Лишние файлы были отброшены.';
+$_lang['ms2_plupload_size'] = 'Размер';
+$_lang['ms2_plupload_status'] = 'Статус';
+$_lang['ms2_plupload_tb'] = 'тб';
+$_lang['ms2_plupload_thumbnails'] = 'Миниатюра';
+$_lang['ms2_plupload_upload_start'] = 'Начать загрузку';
+$_lang['ms2_plupload_upload_stop'] = 'Остановить загрузку';
+$_lang['ms2_plupload_uploaded'] = 'Загружено %d/%d файлов';
+$_lang['ms2_plupload_url_error'] = 'Адрес загрузки неправильный или он не существует.';
