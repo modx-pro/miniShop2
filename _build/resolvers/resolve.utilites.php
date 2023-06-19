47559d4c99689d2193ac4b7eda3cdf293d4bea75
biz87 <info@megawebs.kz>
2023-06-20 00:10:17 +0600

    remove unused logs

--- _build/resolvers/resolve.utilites.php
+++ _build/resolvers/resolve.utilites.php
@@ -12,12 +12,10 @@ if ($transport->xpdo) {
             $menu = $modx->getObject('modMenu', ['text' => 'ms2_utilites']);
             if ($menu) {
                 $menu->remove();
-                $modx->log(modX::LOG_LEVEL_ERROR, "Removed menu item \"ms2_utilites\"");
                 unlink(MODX_ASSETS_PATH . 'components/minishop2/js/mgr/utilites/gallery/panel.js');
                 rmdir(MODX_ASSETS_PATH . 'components/minishop2/js/mgr/utilites/gallery/');
                 unlink(MODX_ASSETS_PATH . 'components/minishop2/js/mgr/utilites/panel.js');
                 rmdir(MODX_ASSETS_PATH . 'components/minishop2/js/mgr/utilites/');
-                $modx->log(modX::LOG_LEVEL_ERROR, "Removed all files in \"utilites\"");
             }
             break;
     }
